79d74bbde1bb6531a48c4afd65abda2399d37168
Mats Tyldum <ma.tyldum@gmail.com>
2020-12-09 10:17:56 +0100

    Added styles folder

---
+++ styles/ThemeProvider.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { useColorScheme } from 'react-native-appearance';
+import { lightColors, darkColors, Colors } from './colorThemes';
+
+export interface Theme {
+  isDark: boolean;
+  colors: Colors;
+  setScheme: (val: 'dark' | 'light') => void;
+}
+
+export const ThemeContext = React.createContext<Theme>({
+  isDark: false,
+  colors: lightColors,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setScheme: () => {},
+});
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const ThemeProvider: React.FC<{}> = (props) => {
+  const colorScheme = useColorScheme();
+  const [isDark, setIsDark] = React.useState<boolean>(colorScheme === 'dark');
+
+  React.useEffect(() => {
+    setIsDark(colorScheme === 'dark');
+  }, [colorScheme]);
+
+  const defaultTheme: Theme = {
+    isDark,
+    colors: isDark ? darkColors : lightColors,
+    setScheme: (scheme) => setIsDark(scheme === 'dark'),
+  };
+
+  // eslint-disable-next-line react/prop-types
+  return <ThemeContext.Provider value={defaultTheme}>{props.children}</ThemeContext.Provider>;
+};
+
+export const useTheme = () => React.useContext(ThemeContext);
