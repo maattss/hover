10534aa6d01b2a1b92a0a85a235a95b5426dcc56
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2020-12-09 13:18:40 +0100

    Feature/leaderboard (#3)
    * leaderbord rewritten to ts w/dummy data
    
    * leaderboard  sorts with score== null
    
    * removed comments
    
    * moved leaderboard to seperat component folder

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line func-names
-export default function (api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
   };
-}
+};
