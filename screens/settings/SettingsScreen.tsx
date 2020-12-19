e779e9a7bd714f13e31a70f328a767e73506a552
Siri Mykland <sirim_mykland@hotmail.com>
2020-12-19 16:02:23 +0100

    edit query

--- screens/settings/SettingsScreen.tsx
+++ screens/settings/SettingsScreen.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, Text, TouchableOpacity, ActivityIndicator, View, TextInput } from 'react-native';
+import { SafeAreaView, Text, TouchableOpacity, ActivityIndicator, View, TextInput, StyleSheet } from 'react-native';
 import tailwind from 'tailwind-rn';
 import { useQuery, useMutation } from '@apollo/client';
 import { Feather } from '@expo/vector-icons';
 
 import Firebase from '../../lib/firebase';
-import { GET_USER, UPDATE_USER_NAME } from '../../lib/queries';
+import { GET_USER, UPDATE_USER_NAME } from '../../lib/queries/settingsQueries';
+import { color } from 'react-native-reanimated';
 
-export function SettingsScreen() {
+const SettingsScreen = () => {
   const id = Firebase.auth().currentUser?.uid;
   const [editing, setEditing] = useState(false);
   const [name, setName] = useState('');
@@ -19,63 +20,39 @@ export function SettingsScreen() {
     if (data) {
       const { users_by_pk: user } = data;
       const { name } = user;
-      {
-        /* if a name already exists, set it as the name */
-      }
       newName = name;
     }
 
     if (response) {
       const { update_users_by_pk } = response;
       const { name } = update_users_by_pk;
-      {
-        /* when a user updates their name, set that as the name */
-      }
       newName = name;
     }
-
     setName(newName);
-    {
-      /* call this effect whenever data or response changes */
-    }
   }, [data, response]);
 
-  {
-    /* Show a spinner if we are fetching/updating the data */
-  }
   if (fetchLoading || mutationLoading)
     return (
-      <SafeAreaView style={tailwind('flex-1 justify-center items-center')}>
-        <ActivityIndicator color={'#0000FF'} />
+      <SafeAreaView style={[styles.flex, styles.center]}>
+        <ActivityIndicator color={'rgba(59, 130, 246, 1)'} />
       </SafeAreaView>
     );
   return (
-    <SafeAreaView style={tailwind('flex-1')}>
-      <View style={tailwind('py-10 px-5')}>
-        <View style={tailwind('flex-row justify-between items-center')}>
-          <Text style={tailwind('text-4xl font-bold')}>Home</Text>
-          <TouchableOpacity
-            style={tailwind('rounded-full bg-red-500 h-10 w-10 items-center justify-center')}
-            onPress={() => Firebase.auth().signOut()}>
-            <Feather name="log-out" color="#fff" size={20} />
-          </TouchableOpacity>
-        </View>
+    <SafeAreaView style={styles.flex}>
+      <View style={styles.container}>
         <View style={tailwind('mt-10')}>
-          {/* If we don't have a name or
-          the user wants to edit their name
-          show the edit form */}
           {(!name || editing) && (
             <View>
-              <Text style={tailwind('text-xl')}>What is your name?</Text>
-              <View style={tailwind('border-b border-blue-500 mt-2 flex-row justify-between items-center pb-2')}>
+              <Text style={styles.textXl}>What is your name?</Text>
+              <View style={styles.row}>
                 <TextInput
-                  placeholder="Not Faraz"
+                  placeholder={!name ? 'Name ...' : name}
                   onChangeText={(val) => setName(val)}
                   onFocus={() => setEditing(true)}
                   style={tailwind('text-2xl flex-grow')}
                 />
                 <TouchableOpacity
-                  style={tailwind('bg-blue-500 h-8 w-8 rounded-full items-center justify-center')}
+                  style={[styles.roundBtn, styles.editBtn]}
                   onPress={() => {
                     updateUser({
                       variables: {
@@ -90,25 +67,82 @@ export function SettingsScreen() {
               </View>
             </View>
           )}
-          {/* If we have a name, display it
-          with an option to edit */}
+
           {name && !editing && (
             <View>
-              <Text style={tailwind('text-xl')}>Did I get your name right?</Text>
-              <View style={tailwind('mt-2 flex-row justify-between items-center pb-2')}>
+              <Text style={styles.textXl}>Did I get your name right?</Text>
+              <View style={styles.row}>
                 <Text style={tailwind('text-2xl text-blue-500')}>{name}</Text>
-                <TouchableOpacity
-                  style={tailwind('bg-blue-500 h-8 w-8 rounded-full items-center justify-center')}
-                  onPress={() => setEditing(true)}>
+                <TouchableOpacity style={[styles.roundBtn, styles.editBtn]} onPress={() => setEditing(true)}>
                   <Feather name="edit" size={16} color="#fff" />
                 </TouchableOpacity>
               </View>
             </View>
           )}
         </View>
+        <View style={[styles.row]}>
+          <TouchableOpacity style={[styles.longBtn, styles.signoutBtn]} onPress={() => Firebase.auth().signOut()}>
+            <Text style={styles.textXl}>
+              Sign out
+              <Feather name="log-out" color="#fff" size={20} />
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
-}
+};
 
-export default SettingsScreen;
+const styles = StyleSheet.create({
+  flex: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  container: {
+    paddingBottom: 50,
+    paddingTop: 50,
+    paddingRight: 20,
+    paddingLeft: 20,
+  },
+  row: {
+    marginTop: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 2,
+  },
+  roundBtn: {
+    height: 40,
+    width: 40,
+    borderRadius: 9999,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  editBtn: {
+    backgroundColor: 'rgba(59, 130, 246, 1)',
+  },
+  longBtn: {
+    height: 50,
+    width: 300,
+    borderRadius: 9999,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  signoutBtn: {
+    backgroundColor: 'rgba(239, 68, 68, 1)',
+  },
+  textXl: {
+    fontSize: 15,
+    lineHeight: 15,
+  },
+  label: {
+    fontSize: 17,
+    flex: 1,
+    paddingRight: 80,
+  },
+});
