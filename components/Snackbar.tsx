7930cb356e0f1f8fb84967e0ab9169f679194185
Mats Tyldum <ma.tyldum@gmail.com>
2021-01-12 09:57:14 +0100

    Update Snackbar.tsx

--- components/Snackbar.tsx
+++ components/Snackbar.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Colors, Typography } from '../theme';
+import { Buttons, Colors, Typography } from '../theme';
 
 interface SnackbarProps {
   variant: snackbarType;
@@ -31,20 +31,31 @@ const getBackgroundColor = (variant: snackbarType) => {
 };
 
 const SnackBar = (props: SnackbarProps) => {
+  const [showSnackBar, setShowSnackBar] = useState(true);
   const snackbarStyles = {
     backgroundColor: getBackgroundColor(props.variant),
   };
   return (
-    <View style={[styles.container, snackbarStyles]}>
-      <Text>{props.title}</Text>
-      <Text>{props.message}</Text>
-    </View>
+    showSnackBar && (
+      <View style={[styles.container, snackbarStyles]}>
+        <Text>{props.title}</Text>
+        <Text>{props.message}</Text>
+        <TouchableOpacity onPress={() => setShowSnackBar(false)} style={{ ...Buttons.button }}>
+          <Text>Close</Text>
+        </TouchableOpacity>
+      </View>
+    )
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
+    position: 'absolute',
+    top: '5%',
+    left: '5%',
+    width: '90%',
+    height: '10%',
   },
   title: {
     ...Typography.headerText,
