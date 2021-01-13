d0704290c1426648a2d3d63415c114a0aa9dbda4
Siri Mykland <sirim_mykland@hotmail.com>
2021-01-13 14:31:46 +0100

    introducing task manager

--- App.tsx
+++ App.tsx
@@ -1,14 +1,6 @@
 import { ApolloProvider } from '@apollo/client';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import React from 'react';
-=======
-import React, { useEffect } from 'react';
->>>>>>>  task manager updates location and verifies geofence position
 import { Platform, StatusBar } from 'react-native';
-=======
-import React from 'react';
-import { StatusBar } from 'react-native';
 import { AppearanceProvider } from 'react-native-appearance';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AuthProvider from './components/AuthProvider';
@@ -38,3 +30,33 @@ export default function App() {
     </AuthProvider>
   );
 }
+const LOCATION_BACKGROUND_TRACKING = 'location-background-tracking';
+
+TaskManager.defineTask(LOCATION_BACKGROUND_TRACKING, async ({ data: { locations }, error }) => {
+  console.log('taskmanager', LOCATION_BACKGROUND_TRACKING, 'running', 'OS:', Platform.OS);
+  if (error) {
+    console.log('LOCATION_BACKGROUND_TRACKING task ERROR:', error);
+    return;
+  }
+  if (locations) {
+    console.log('Received new locations', locations);
+    const lat = locations[0].coords.latitude;
+    const long = locations[0].coords.longitude;
+
+    console.log(`${new Date(Date.now()).toLocaleString()}: ${lat},${long}`);
+  }
+});
+
+const LOCATION_GEOFENCING_EVENT = 'location-geofencing-event';
+TaskManager.defineTask(LOCATION_GEOFENCING_EVENT, ({ data: { eventType, region }, error }) => {
+  if (error) {
+    // check `error.message` for more details.
+    console.error('Error:', error.code, error.message);
+    return;
+  }
+  if (eventType === LocationGeofencingEventType.Enter) {
+    console.log("You've entered region:", region);
+  } else if (eventType === LocationGeofencingEventType.Exit) {
+    console.log("You've left region:", region);
+  }
+});
