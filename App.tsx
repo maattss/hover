d527d9a2027d2641eb1095a50f95657c1e14a473
Siri Mykland <sirim_mykland@hotmail.com>
2021-01-13 17:00:21 +0100

    task manager updates location and verifies geofence position

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { ApolloProvider } from '@apollo/client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Platform, StatusBar } from 'react-native';
 import { AppearanceProvider } from 'react-native-appearance';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -8,24 +8,48 @@ import { apolloClient } from './lib/apollo';
 import AppNavigation from './navigation/RootNavigation';
 import * as Location from 'expo-location';
 import * as TaskManager from 'expo-task-manager';
-import { LocationGeofencingEventType, LocationObject } from 'expo-location';
-
+import * as Permissions from 'expo-permissions';
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const [location, setLocation] = useState<LocationObject | null>(null);
 
   useEffect(() => {
     (async () => {
-      const { status } = await Location.requestPermissionsAsync();
-      if (status !== 'granted') {
-        console.log('Permission to access location was denied');
-        return;
+      const { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status === 'granted') {
+        return startLocationTracking();
+      } else {
+        throw new Error('Location permission not granted');
       }
-
-      setLocation(await Location.getCurrentPositionAsync({}));
     })();
   }, []);
 
+  const startLocationTracking = async () => {
+    await Location.startLocationUpdatesAsync(LOCATION_BACKGROUND_TRACKING, {
+      accuracy: Location.Accuracy.Highest,
+      timeInterval: 6000,
+    });
+    const regions: Location.LocationRegion[] = [
+      {
+        identifier: '1',
+        latitude: 34.17,
+        longitude: -118.4,
+        radius: 50,
+        notifyOnEnter: true,
+        notifyOnExit: true,
+      },
+      {
+        identifier: '2',
+        latitude: 63.4155446,
+        longitude: 10.4044603,
+        radius: 50,
+        notifyOnEnter: true,
+        notifyOnExit: true,
+      },
+    ];
+    await Location.startGeofencingAsync(LOCATION_GEOFENCING_EVENT, regions);
+    const hasStarted = await Location.hasStartedLocationUpdatesAsync(LOCATION_BACKGROUND_TRACKING);
+    console.log('hasStartedLocationUpdatesAsync:', hasStarted);
+  };
   if (!isLoadingComplete) {
     return null;
   }
@@ -43,30 +67,29 @@ export default function App() {
 const LOCATION_BACKGROUND_TRACKING = 'location-background-tracking';
 
 TaskManager.defineTask(LOCATION_BACKGROUND_TRACKING, async ({ data: { locations }, error }) => {
-  console.log('taskmanager', LOCATION_BACKGROUND_TRACKING, 'running', 'OS:', Platform.OS);
+  console.log('TaskManager', LOCATION_BACKGROUND_TRACKING, 'running', 'OS:', Platform.OS);
   if (error) {
-    console.log('LOCATION_BACKGROUND_TRACKING task ERROR:', error);
+    console.log('LOCATION_BACKGROUND_TRACKING task ERROR:', error.message);
     return;
   }
   if (locations) {
-    console.log('Received new locations', locations);
-    const lat = locations[0].coords.latitude;
-    const long = locations[0].coords.longitude;
+    const { coords, timestamp } = locations[0];
+    const { latitude, longitude } = coords;
 
-    console.log(`${new Date(Date.now()).toLocaleString()}: ${lat},${long}`);
+    console.log(`${timestamp}: ${latitude},${longitude}`);
   }
 });
 
-const LOCATION_GEOFENCING_EVENT = 'location-geofencing-event';
+export const LOCATION_GEOFENCING_EVENT = 'location-geofencing-event';
 TaskManager.defineTask(LOCATION_GEOFENCING_EVENT, ({ data: { eventType, region }, error }) => {
   if (error) {
     // check `error.message` for more details.
     console.error('Error:', error.code, error.message);
     return;
   }
-  if (eventType === LocationGeofencingEventType.Enter) {
+  if (eventType === Location.LocationGeofencingEventType.Enter) {
     console.log("You've entered region:", region);
-  } else if (eventType === LocationGeofencingEventType.Exit) {
+  } else if (eventType === Location.LocationGeofencingEventType.Exit) {
     console.log("You've left region:", region);
   }
 });
