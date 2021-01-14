a82328645f083df31759d5f0ab56c884f6756f47
Siri Mykland <sirim_mykland@hotmail.com>
2021-01-14 15:38:52 +0100

    Added three types of location tracking

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { ApolloProvider } from '@apollo/client';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react';
 =======
 import React, { useEffect } from 'react';
 >>>>>>>  task manager updates location and verifies geofence position
 import { Platform, StatusBar } from 'react-native';
+=======
+import React from 'react';
+import { StatusBar } from 'react-native';
 import { AppearanceProvider } from 'react-native-appearance';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AuthProvider from './components/AuthProvider';
@@ -34,32 +38,3 @@ export default function App() {
     </AuthProvider>
   );
 }
-const LOCATION_BACKGROUND_TRACKING = 'location-background-tracking';
-
-TaskManager.defineTask(LOCATION_BACKGROUND_TRACKING, async ({ data: { locations }, error }) => {
-  console.log('TaskManager', LOCATION_BACKGROUND_TRACKING, 'running', 'OS:', Platform.OS);
-  if (error) {
-    console.log('LOCATION_BACKGROUND_TRACKING task ERROR:', error.message);
-    return;
-  }
-  if (locations) {
-    const { coords, timestamp } = locations[0];
-    const { latitude, longitude } = coords;
-
-    console.log(`${timestamp}: ${latitude},${longitude}`);
-  }
-});
-
-export const LOCATION_GEOFENCING_EVENT = 'location-geofencing-event';
-TaskManager.defineTask(LOCATION_GEOFENCING_EVENT, ({ data: { eventType, region }, error }) => {
-  if (error) {
-    // check `error.message` for more details.
-    console.error('Error:', error.code, error.message);
-    return;
-  }
-  if (eventType === Location.LocationGeofencingEventType.Enter) {
-    console.log("You've entered region:", region);
-  } else if (eventType === Location.LocationGeofencingEventType.Exit) {
-    console.log("You've left region:", region);
-  }
-});
