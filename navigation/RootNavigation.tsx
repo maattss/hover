9cbcedafe745e2298fab45d7d9242b327e39fe61
Mats Tyldum <ma.tyldum@gmail.com>
2021-01-14 16:25:17 +0100

    Dark mode init

--- navigation/RootNavigation.tsx
+++ navigation/RootNavigation.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { RootStackParamList } from '../types/navigationTypes';
 import TabNavigator from './TabNavigator';
