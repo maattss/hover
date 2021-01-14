7c2197b3b0405cc006727e815bacf202b4f03378
Mats Tyldum <ma.tyldum@gmail.com>
2021-01-14 09:01:16 +0100

    Updated query

--- lib/queries/geoFenceQueries.ts
+++ lib/queries/geoFenceQueries.ts
@@ -1,12 +1,14 @@
 import { gql } from '@apollo/client';
 
-// TODO: Update
 export const GET_GEOFENCES = gql`
   query($id: String!) {
-    users_by_pk(id: $id) {
-      id
-      name
-      bio
+    geofences {
+      category
+      coordinates
+      latitude
+      longitude
+      radius
+      variant
     }
   }
 `;
