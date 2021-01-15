98f0f005a2712451a61cf5383c9183fc69982e7f
Siri Mykland <sirim_mykland@hotmail.com>
2021-01-15 14:18:32 +0100

    changed exit area task to have region as prop

--- tasks/ExitAreaTrackingTask.tsx
+++ tasks/ExitAreaTrackingTask.tsx
@@ -4,22 +4,13 @@ import { checkMultiPermissions } from '../helpers/checkPermissions';
 import { EXIT_AREA_TRACKING } from '../tasks';
 
 // This task checks if user leaves a region
-const ExitAreaTrackingTask = () => {
+const ExitAreaTrackingTask = (regions: Location.LocationRegion[]) => {
   useEffect(() => {
-    checkMultiPermissions().then(() => startLocationTracking());
+    if (regions) {
+      checkMultiPermissions().then(() => startLocationTracking());
+    }
   }, []);
   const startLocationTracking = async () => {
-    const regions: Location.LocationRegion[] = [
-      {
-        // name: 'Samfundet',
-        identifier: '1',
-        latitude: 63.4225,
-        longitude: 10.3952,
-        radius: 50,
-        notifyOnEnter: false,
-        notifyOnExit: true,
-      },
-    ];
     await Location.startGeofencingAsync(EXIT_AREA_TRACKING, regions);
   };
   return <></>;
