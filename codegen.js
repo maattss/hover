dad2762bf9a255c9c83371d6dec6ee92f7d9a2dd
Mats Tyldum <ma.tyldum@gmail.com>
2021-01-15 14:36:56 +0100

    Init codegen

---
+++ codegen.js
@@ -0,0 +1,27 @@
+module.exports = {
+  schema: [
+    {
+      'https://hasura.io/learn/graphql': {
+        headers: {
+          Authorization: 'Bearer ' + process.env.AUTH_TOKEN,
+        },
+      },
+    },
+  ],
+  documents: ['./src/**/*.tsx', './src/**/*.ts'],
+  overwrite: true,
+  generates: {
+    './src/generated/graphql.tsx': {
+      plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
+      config: {
+        skipTypename: false,
+        withHooks: true,
+        withHOC: false,
+        withComponent: false,
+      },
+    },
+    './graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
+};
