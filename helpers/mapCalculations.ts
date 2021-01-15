0db9bfcce48493dc78f40e4c516dbb6b5457dd38
Siri Mykland <sirim_mykland@hotmail.com>
2021-01-15 14:21:19 +0100

    added functions fro conerting Geofence to geofence region and function for estimated radius

--- helpers/mapCalculations.ts
+++ helpers/mapCalculations.ts
@@ -1,5 +1,6 @@
-import { LatLng } from 'react-native-maps';
-import { PolygonGeoFence, CircleGeoFence } from 'types/geoFenceTypes';
+import { Coordinate, LatLng } from 'react-native-maps';
+import { PolygonGeoFence, CircleGeoFence } from '../types/geoFenceTypes';
+import { LocationRegion } from 'expo-location';
 
 export const isInsideCircle = (userLocation: LatLng, geoFence: CircleGeoFence) => {
   const distance = measureCircleDistance(
@@ -34,4 +35,48 @@ export const isInsidePolygon = (userLocation: LatLng, geoFence: PolygonGeoFence)
   const insidePolygon = classifyPoint(polygon, [userLocation.latitude, userLocation.longitude]);
   if (insidePolygon === -1 || insidePolygon === 0) return true;
   return false;
+};
+
+export const estimatedRadius = (coordinates: Coordinate[]) => {
+  let biggestLength = 0;
+  let length: number;
+  coordinates.forEach((coordinateA) => {
+    coordinates.forEach((coordinateB) => {
+      length = measureCircleDistance(coordinateA[0], coordinateA[1], coordinateB[0], coordinateB[1]);
+      if (length > biggestLength) biggestLength = length;
+    });
+  });
+  return Math.sqrt(biggestLength);
+};
+
+export const convertToRegion = (geofences: any): LocationRegion[] => {
+  const fetchedGeoFences: LocationRegion[] = [];
+  for (const obj of geofences) {
+    if (obj.variant === 'CIRCLE') {
+      fetchedGeoFences.push({
+        identifier: obj.id,
+        latitude: obj.latitude,
+        longitude: obj.longitude,
+        radius: obj.radius,
+        notifyOnEnter: true,
+        notifyOnExit: true,
+      });
+    } else if (obj.variant === 'POLYGON') {
+      const coordinatesRaw: string = obj.coordinates.split(',');
+      const coordinates: Coordinate[] = [];
+      for (let i = 0; i < coordinatesRaw.length; i = i + 2) {
+        coordinates.push([+coordinatesRaw[i], +coordinatesRaw[i + 1]]);
+      }
+      console.log('Radius:', obj.radius, '      EstimatedRadius:', estimatedRadius(coordinates));
+      fetchedGeoFences.push({
+        identifier: obj.id,
+        latitude: obj.latitude,
+        longitude: obj.longitude,
+        radius: obj.radius ? obj.radius : estimatedRadius(coordinates),
+        notifyOnEnter: true,
+        notifyOnExit: true,
+      });
+    }
+  }
+  return fetchedGeoFences;
 };
