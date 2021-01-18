5e450bbdf4c3ca0afafe94eda4da3d00f2b4901a
Mats Tyldum <ma.tyldum@gmail.com>
2021-01-18 08:55:35 +0100

    Update query/mutation names and corresponding types

--- lib/mutations/settingsMutations.ts
+++ lib/mutations/settingsMutations.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const UPDATE_USER_NAME = gql`
-  mutation($id: String!, $name: String!, $bio: String) {
+  mutation updateUser($id: String!, $name: String!, $bio: String) {
     update_users_by_pk(pk_columns: { id: $id }, _set: { name: $name, bio: $bio }) {
       id
       name
