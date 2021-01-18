c41533d3a82095c5a932017b64077bab1be834cb
Mats Tyldum <ma.tyldum@gmail.com>
2021-01-18 09:11:31 +0100

    Fix lint errors and added eslint ignore to generated types file

--- navigation/RootNavigation.tsx
+++ navigation/RootNavigation.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NavigationContainer, Theme } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { User } from 'firebase';
 import Firebase from '../lib/firebase';
