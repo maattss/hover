b4bdb7316bbc09a7cdffa12e921534f49b20fb0c
Siri Mykland <sirim_mykland@hotmail.com>
2021-01-18 11:20:37 +0100

    application permission changed

--- navigation/RootNavigation.tsx
+++ navigation/RootNavigation.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { LOCATION, usePermissions } from 'expo-permissions';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { User } from 'firebase';
@@ -8,8 +9,43 @@ import { RootStackParamList } from '../types/navigationTypes';
 import TabNavigator from './TabNavigator';
 import SignupScreen from '../screens/auth/SignUpScreen';
 import LoginScreen from '../screens/auth/LoginScreen';
+import NotifyWithinAreaTask from '../tasks/NotifyWithinAreaTask';
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native';
+import { Colors } from '../theme/colors';
+import { Buttons } from '../theme';
 
 export const RootStack = createStackNavigator<RootStackParamList>();
+
+const MainContainer: React.FC = () => {
+  const [permission, askPermission] = usePermissions(LOCATION);
+
+  if (permission?.granted) {
+    return (
+      <>
+        <NotifyWithinAreaTask />
+        <TabNavigator />
+      </>
+    );
+  }
+
+  return (
+    <View style={styles.page}>
+      <View style={styles.box}>
+        <Text style={styles.title}>We need your permission</Text>
+        <Text style={styles.paragraph}>To monitor your activity, we need access to background location.</Text>
+      </View>
+      {!permission ? (
+        <ActivityIndicator color={Colors.primary} />
+      ) : (
+        <View style={styles.center}>
+          <TouchableOpacity onPress={askPermission}>
+            <Text style={{ ...Buttons.buttonText }}>Grant permission</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </View>
+  );
+};
 
 const AppNavigation: React.FC = () => {
   const [userAuthState, setUserAuthState] = useState<User | null>(null);
@@ -28,7 +64,7 @@ const AppNavigation: React.FC = () => {
     <NavigationContainer>
       <RootStack.Navigator screenOptions={{ headerShown: false }}>
         {userAuthState ? (
-          <RootStack.Screen name="Main" component={TabNavigator} />
+          <RootStack.Screen name="Main" component={MainContainer} />
         ) : (
           <>
             <RootStack.Screen name="Login" component={LoginScreen} />
@@ -41,3 +77,46 @@ const AppNavigation: React.FC = () => {
 };
 
 export default AppNavigation;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: Colors.background,
+    padding: 4,
+  },
+  box: {
+    paddingVertical: 1,
+  },
+  center: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 36,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  paragraph: {
+    fontSize: 18,
+    lineHeight: 26,
+    paddingVertical: 6,
+  },
+  button: {
+    color: 'white',
+    fontFamily: 'open-sans',
+    fontWeight: 'bold',
+  },
+  buttons: {
+    backgroundColor: 'black',
+    borderRadius: 2,
+    paddingHorizontal: 3,
+    paddingVertical: 2,
+  },
+});
