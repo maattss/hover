5d446bad54a1f458af418122bd330e23dd85319a
Siri Mykland <sirim_mykland@hotmail.com>
2021-01-18 20:50:34 +0100

    removed unused imports

--- navigation/RootNavigation.tsx
+++ navigation/RootNavigation.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { RootStackParamList } from '../types/navigationTypes';
