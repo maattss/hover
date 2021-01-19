fe59556c6aaf1094eb45cfb6d2d9bc51de3f68eb
Mats Tyldum <ma.tyldum@gmail.com>
2021-01-19 09:04:32 +0100

    PR improvements

--- theme/colors.ts
+++ theme/colors.ts
@@ -35,15 +35,3 @@ export const DarkTheme: Theme = {
     notification: red,
   },
 };
-
-export const LightTheme: Theme = {
-  dark: false,
-  colors: {
-    primary: blue,
-    background: 'rgb(242, 242, 242)',
-    card: 'rgb(255, 255, 255)',
-    text: 'rgb(28, 28, 30)',
-    border: 'rgb(216, 216, 216)',
-    notification: 'rgb(255, 59, 48)',
-  },
-};
