e02d8ac82f545b6abec0f2584397e775b5d6f777
Mats Tyldum <ma.tyldum@gmail.com>
2021-01-25 12:42:45 +0100

    Updates

--- components/TrackingProvider.tsx
+++ components/TrackingProvider.tsx
@@ -1,15 +1,23 @@
-import React, { useEffect, useState, ReactNode, SetStateAction } from 'react';
+import React, { useState, ReactNode, useEffect } from 'react';
 import { LatLng } from 'react-native-maps';
 import { GeoFence, TrackedActivity } from '../types/geoFenceTypes';
+import { convertToGeoFence } from '../helpers/objectMappers';
+import { useGeofencesQuery } from '../graphql/queries/Geofences.generated';
+import { usePermissions, LOCATION, PermissionResponse } from 'expo-permissions';
+import { startBackgroundUpdate } from '../tasks/locationBackgroundTasks';
+import { startGeofencing } from '../tasks/geofenceTasks';
 
 interface Props {
   children: ReactNode;
 }
 
 interface TrackingContextValues {
+  locationPermission: PermissionResponse | undefined;
   userLocation: LatLng | null;
+  setUserLocation: React.Dispatch<React.SetStateAction<LatLng | null>> | null;
+  geoFences: GeoFence[];
   completedActivities: TrackedActivity[];
-  addCompletedActivity: (activity: TrackedActivity) => void | null;
+  addCompletedActivity: ((activity: TrackedActivity) => void) | null;
   insideGeoFence: GeoFence | null;
   setInsideGeoFence: React.Dispatch<React.SetStateAction<GeoFence | null>> | null;
   isTracking: boolean;
@@ -23,7 +31,10 @@ interface TrackingContextValues {
 }
 
 export const TrackingContext = React.createContext<TrackingContextValues>({
+  locationPermission: undefined,
   userLocation: null,
+  setUserLocation: null,
+  geoFences: [],
   completedActivities: [],
   addCompletedActivity: null,
   insideGeoFence: null,
@@ -39,8 +50,18 @@ export const TrackingContext = React.createContext<TrackingContextValues>({
 });
 TrackingContext.displayName = 'TrackingContext';
 
+// const { data: data } = useGeofencesQuery();
+// // TODO: Remove
+// useEffect(() => {
+//   getLocationsPermissions();
+//   startBackgroundUpdate();
+//   if (data) startGeofencing(data);
+// }, [data]);
+
 export const TrackingProvider = ({ children }: Props) => {
+  const [locationPermission] = usePermissions(LOCATION, { ask: true });
   const [userLocation, setUserLocation] = useState<LatLng | null>(null);
+  const [geoFences, setGeoFences] = useState<GeoFence[]>([]);
   const [completedActivities, setCompletedActivities] = useState<TrackedActivity[]>([]);
   const [insideGeoFence, setInsideGeoFence] = useState<GeoFence | null>(null);
   const [isTracking, setIsTracking] = useState(false);
@@ -48,11 +69,28 @@ export const TrackingProvider = ({ children }: Props) => {
   const [score, setScore] = useState(0);
   const [duration, setDuration] = useState(0);
 
+  const { error: fetchError, data: data } = useGeofencesQuery();
+
+  useEffect(() => {
+    if (locationPermission && locationPermission.status !== 'granted') {
+      startBackgroundUpdate();
+      if (data) startGeofencing(data);
+    }
+  }, [data, locationPermission]);
+
+  useEffect(() => {
+    if (data) setGeoFences(convertToGeoFence(data));
+    if (fetchError) console.error(fetchError.message);
+  }, [data, fetchError]);
+
   const addCompletedActivity = (activity: TrackedActivity) =>
     setCompletedActivities([...completedActivities, activity]);
 
   const value: TrackingContextValues = {
+    locationPermission: locationPermission,
     userLocation: userLocation,
+    setUserLocation: setUserLocation,
+    geoFences: geoFences,
     completedActivities: completedActivities,
     addCompletedActivity: addCompletedActivity,
     insideGeoFence: insideGeoFence,
