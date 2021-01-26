df141c6b87ce7e5c91aa7afa2a6cc4eee7c1b32a
Siri Mykland <sirim_mykland@hotmail.com>
2021-01-26 17:10:07 +0100

    Picker works flwalessly on ios

--- screens/statistics/LeaderboardScreen.tsx
+++ screens/statistics/LeaderboardScreen.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, ReactText, SetStateAction, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Leaderboard, { Item } from '../../components/Leaderboard';
 import { ActivityIndicator, StyleSheet, Text, View, TouchableOpacity, TextStyle, ViewStyle } from 'react-native';
 import { HighscoreQueryVariables, useHighscoreQuery } from '../../graphql/queries/Highscore.generated';
@@ -6,8 +6,10 @@ import { Buttons, Colors, Spacing, Typography } from '../../theme';
 import { convertToHighscoreList } from '../../helpers/objectMappers';
 import { Picker } from '@react-native-picker/picker';
 import { PickerItemProps } from '@react-native-picker/picker/typings/Picker';
+import { FontAwesome5 as FAIcon } from '@expo/vector-icons';
+import { red } from '../../theme/colors';
 
-const STATIC_CATEGORIES = [
+const STATIC_CATEGORIES: PickerItemProps[] = [
   { label: 'All Categories', value: '' },
   { label: 'Culture', value: 'CULTURE' },
   { label: 'Education', value: 'EDUCATION' },
@@ -15,15 +17,27 @@ const STATIC_CATEGORIES = [
   { label: 'Social', value: 'SOCIAL' },
 ];
 
+const STATIC_TIMESPAN: PickerItemProps[] = [
+  { label: 'All Time', value: '' },
+  { label: 'Today', value: Date() },
+  { label: 'This Week', value: Date() },
+  { label: 'This Month', value: Date() },
+  { label: 'This Year', value: Date() },
+];
+
 const LeaderboardScreen: React.FC = () => {
   const [category, setCategory] = useState<number | string>('');
   const [editCategory, setEditCategory] = useState(false);
+  const [timespan, setTimespan] = useState<number | string>('');
+  const [editTimespan, setEditTimespan] = useState(false);
 
   const { data: highscoreData, loading: highscoreLoading, error: highscoreError, refetch } = useHighscoreQuery({
-    variables: { category: category !== '' ? category : null } as HighscoreQueryVariables,
+    variables: {
+      category: category !== '' ? category : null,
+      timespan: timespan !== '' ? timespan : null,
+    } as HighscoreQueryVariables,
   });
   const [highscores, setHighscores] = useState<Item[]>([]);
-  const [allCategories] = useState<PickerItemProps[]>(STATIC_CATEGORIES);
 
   useEffect(() => {
     if (highscoreData) {
@@ -36,10 +50,31 @@ const LeaderboardScreen: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.buttonsContainer}>
+      <View style={styles.filterContainer}>
         {setCategory && (
-          <TouchableOpacity style={styles.categoryButton} onPress={() => setEditCategory(true)}>
-            <Text style={{ ...Buttons.buttonText }}>{allCategories.find((obj) => obj.value === category)?.label}</Text>
+          <TouchableOpacity
+            style={styles.filterButton}
+            onPress={() => {
+              setEditCategory(true);
+              setEditTimespan(false);
+            }}>
+            <Text style={{ ...Buttons.buttonText }}>
+              {STATIC_CATEGORIES.find((obj) => obj.value === category)?.label}
+            </Text>
+            <FAIcon name="arrow-down" style={styles.filterIcon} />
+          </TouchableOpacity>
+        )}
+        {setTimespan && (
+          <TouchableOpacity
+            style={styles.filterButton}
+            onPress={() => {
+              setEditTimespan(true);
+              setEditCategory(false);
+            }}>
+            <Text style={{ ...Buttons.buttonText }}>
+              {STATIC_TIMESPAN.find((obj) => obj.value === timespan)?.label}
+            </Text>
+            <FAIcon name="arrow-down" style={styles.filterIcon} />
           </TouchableOpacity>
         )}
       </View>
@@ -47,14 +82,25 @@ const LeaderboardScreen: React.FC = () => {
         {highscores && <Leaderboard data={highscores} refetch={refetch} />}
       </View>
 
-      {editCategory && (
-        <CategoryPicker
-          items={allCategories}
+      {editCategory && !editTimespan && (
+        <FilterPicker
+          items={STATIC_CATEGORIES}
           selectedValue={category}
           onValueChange={(value) => {
             setCategory(value);
             refetch();
             setEditCategory(false);
+          }}
+        />
+      )}
+      {editTimespan && !editCategory && (
+        <FilterPicker
+          items={STATIC_TIMESPAN}
+          selectedValue={timespan}
+          onValueChange={(value) => {
+            setTimespan(value);
+            refetch();
+            setEditTimespan(false);
           }}
         />
       )}
@@ -68,15 +114,15 @@ interface PickerProps {
   items: PickerItemProps[];
   selectedValue: number | string;
   onValueChange: (value: number | string) => void;
-  setCategory?: Dispatch<SetStateAction<ReactText>>;
 }
 
-const CategoryPicker = ({ items, selectedValue, onValueChange }: PickerProps) => {
+const FilterPicker = ({ items, selectedValue, onValueChange }: PickerProps) => {
   return (
     <View style={styles.pickerContainer}>
       <Picker
-        mode="dialog"
-        style={[styles.infoText, { width: 150 }]}
+        mode="dropdown"
+        prompt="Choose a filter"
+        style={styles.picker}
         selectedValue={selectedValue}
         onValueChange={(itemValue) => onValueChange(itemValue)}>
         {items.map((item) => (
@@ -89,34 +135,37 @@ const CategoryPicker = ({ items, selectedValue, onValueChange }: PickerProps) =>
 
 interface StylesProps {
   container: ViewStyle;
-  buttonsContainer: ViewStyle;
+  filterContainer: ViewStyle;
   leaderboardContainer: ViewStyle;
   pickerContainer: ViewStyle;
   refreshContainer: ViewStyle;
   infoText: TextStyle;
   picker: TextStyle;
-  categoryButton: ViewStyle;
+  filterButton: ViewStyle;
+  filterIcon: TextStyle;
   refreshButton: ViewStyle;
 }
 const styles: StylesProps = StyleSheet.create({
   container: {
-    height: '100%',
-    width: '100%',
     flex: 1,
+    display: 'flex',
+    alignItems: 'center',
   },
-  buttonsContainer: {
+  filterContainer: {
+    width: '100%',
     display: 'flex',
     flexDirection: 'row',
-    backgroundColor: Colors.red,
+    justifyContent: 'space-between',
   },
   leaderboardContainer: {
-    height: '60%',
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
   pickerContainer: {
-    height: '20%',
-    padding: Spacing.base,
+    flex: 1,
+    width: '100%',
+    backgroundColor: Colors.gray500,
     justifyContent: 'center',
     alignItems: 'center',
     textAlign: 'center',
@@ -131,24 +180,22 @@ const styles: StylesProps = StyleSheet.create({
     paddingTop: Spacing.base,
   },
   picker: {
-    ...Typography.bodyText,
     paddingTop: Spacing.base,
-  },
-  pickerItem: {
-    ...Typography.bodyText,
-    paddingTop: Spacing.base,
-  },
-  categoryButton: {
-    ...Buttons.button,
-    backgroundColor: Colors.red,
     width: '100%',
-    marginTop: Spacing.base,
-    marginBottom: Spacing.base,
+    flex: 1,
+  },
+  filterButton: {
+    ...Buttons.button,
+    backgroundColor: Colors.transparent,
+    flexDirection: 'row',
+  },
+  filterIcon: {
+    ...Buttons.buttonText,
+    paddingLeft: Spacing.small,
   },
   refreshButton: {
     ...Buttons.button,
     backgroundColor: Colors.red,
-    width: '100%',
     marginTop: Spacing.base,
     marginBottom: Spacing.base,
   },
