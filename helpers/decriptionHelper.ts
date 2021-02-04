3fd59e957dbdd683845c3d98770696326370ccb6
Siri Mykland <sirim_mykland@hotmail.com>
2021-02-04 13:51:10 +0100

    typo

--- helpers/decriptionHelper.ts
+++ helpers/decriptionHelper.ts
@@ -24,7 +24,7 @@ const scoreDescription = (challenge: PendingChallenge) => {
   let description = challenge.opponents[0].name;
   description += ' challenge you to a Score challenge!';
   description += challenge.rules ? ' Be the first person to reach ' + challenge.rules.score + ' points' : '';
-  description += challenge.end_date ? ' by the ' + toPrettyDate(challenge.end_date) + '.' : '';
+  description += challenge.end_date ? ' by ' + toPrettyDate(challenge.end_date) + '.' : '';
   return description;
 };
 
@@ -33,13 +33,13 @@ const scoreCategoryDescription = (challenge: PendingChallenge) => {
   description += ' challenge you to a Category challenge!';
   description += challenge.rules.score ? ' Be the first person to reach ' + challenge.rules.score + ' points' : '';
   description += challenge.rules.category ? ' in the ' + challenge.rules.category.toLowerCase() + ' category' : '';
-  description += challenge.end_date ? ' by the ' + toPrettyDate(challenge.end_date) + '.' : '';
+  description += challenge.end_date ? ' by ' + toPrettyDate(challenge.end_date) + '.' : '';
   return description;
 };
 
 const defaultDescription = (challenge: PendingChallenge) => {
   let description = challenge.opponents[0].name + ' challenge you to a ';
   description += challenge.challenge_type + ' challenge!';
-  description += challenge.end_date ? ' by the ' + toPrettyDate(challenge.end_date) + '.' : '';
+  description += challenge.end_date ? ' by ' + toPrettyDate(challenge.end_date) + '.' : '';
   return description;
 };
