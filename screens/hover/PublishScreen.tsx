7b5e4bdcbe041e1b73a62c06075cddd922b2fdcf
Mats Tyldum <ma.tyldum@gmail.com>
2021-02-04 10:41:44 +0100

    Update publish screen

--- screens/hover/PublishScreen.tsx
+++ screens/hover/PublishScreen.tsx
@@ -1,11 +1,13 @@
 import { StackNavigationProp } from '@react-navigation/stack';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Alert } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import useTracking from '../../hooks/useTracking';
 import { Buttons, Colors, Spacing, Typography } from '../../theme';
 import { HoverStackParamList } from '../../types/navigationTypes';
 import { FontAwesome5 as FAIcon } from '@expo/vector-icons';
+import { getCategoryColor, getCategoryIconName } from '../../components/feed/ActivityFeedCard';
+import { GeoFenceCategory } from '../../types/geoFenceTypes';
 
 type NavigationProp = StackNavigationProp<HoverStackParamList>;
 
@@ -15,6 +17,7 @@ type ExploreProps = {
 
 const PublishScreen: React.FC<ExploreProps> = ({ navigation }: ExploreProps) => {
   const tracking = useTracking();
+  const [caption, setCaption] = useState('');
 
   const resumeTracking = () => {
     tracking.startTracking();
@@ -34,27 +37,54 @@ const PublishScreen: React.FC<ExploreProps> = ({ navigation }: ExploreProps) => 
     ]);
   };
   const publishActivity = () => {
-    tracking.stopTracking();
+    tracking.stopTracking('Test caption');
     navigation.navigate('Explore');
+  };
+  const categoryColor = {
+    color: getCategoryColor(GeoFenceCategory.EDUCATION),
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={{ ...Typography.headerText, marginTop: Spacing.base }}>Publish</Text>
-      <View style={styles.trackingInfoContainer}>
-        <Text style={styles.scoreText}>Duration: {tracking.duration}</Text>
-        <Text style={styles.scoreText}>Score: {Math.floor(tracking.score)}</Text>
-      </View>
       <View style={styles.resumeButtonContainer}>
         <TouchableOpacity style={[styles.trackingButton, { backgroundColor: Colors.green }]} onPress={resumeTracking}>
           <Text style={styles.trackingButtonText}>Resume</Text>
         </TouchableOpacity>
-      </View>
-      <View style={styles.publishButtonContainer}>
         <TouchableOpacity style={styles.discardButton} onPress={discardActivity}>
-          <Text style={styles.publishButtonText}>Discard</Text>
-          {/* <FAIcon name={'trash'} size={'large'} /> */}
+          <FAIcon name={'trash'} color={Colors.almostWhite} style={{ fontSize: 25 }} />
         </TouchableOpacity>
+      </View>
+      <View style={styles.trackingInfoContainer}>
+        <View style={styles.infoContainer}>
+          <Text style={{ ...Typography.headerText }}>Started</Text>
+          <Text style={{ ...Typography.headerText }}>{tracking.trackingStart}</Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={{ ...Typography.headerText }}>Duration</Text>
+          <Text style={styles.infoNumber}>{tracking.duration}</Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={{ ...Typography.headerText }}>Score</Text>
+          <Text style={styles.infoNumber}>{Math.floor(tracking.score)}</Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={{ ...Typography.headerText }}>Category</Text>
+          <FAIcon style={[styles.categoryIcon, categoryColor]} name={getCategoryIconName(GeoFenceCategory.EDUCATION)} />
+        </View>
+        <View>
+          <Text style={{ ...Typography.headerText }}>Caption</Text>
+          <TextInput
+            placeholder="Insert a funny text that describes the activity!"
+            placeholderTextColor={Colors.gray600}
+            onChangeText={(val) => setCaption(val)}
+            style={styles.formField}
+            numberOfLines={3}>
+            {caption}
+          </TextInput>
+        </View>
+      </View>
+
+      <View style={styles.publishButtonContainer}>
         <TouchableOpacity style={styles.publishButton} onPress={publishActivity}>
           <Text style={styles.publishButtonText}>Publish</Text>
         </TouchableOpacity>
@@ -67,20 +97,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     margin: Spacing.smaller,
+    justifyContent: 'space-between',
+  },
+  categoryIcon: {
+    color: Colors.almostWhite,
+    fontSize: 40,
+    textAlign: 'center',
+    width: '100%',
+    marginVertical: Spacing.smallest,
   },
   trackingInfoContainer: {
-    height: '30%',
-    padding: Spacing.smallest,
+    padding: Spacing.base,
     marginBottom: Spacing.smallest,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Colors.gray900,
+    // backgroundColor: Colors.gray900,
     borderRadius: Spacing.smaller,
   },
-  scoreText: {
+  infoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: Spacing.small,
+  },
+  infoNumber: {
     ...Typography.headerText,
-    marginTop: Spacing.base,
-    marginBottom: Spacing.base,
+    marginRight: Spacing.base,
   },
   resumeButtonContainer: {
     flexDirection: 'row',
@@ -101,20 +140,34 @@ const styles = StyleSheet.create({
   },
   publishButtonContainer: {
     flexDirection: 'row',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     margin: Spacing.base,
   },
   publishButton: {
     ...Buttons.button,
+    paddingHorizontal: Spacing.extraLarge,
   },
   discardButton: {
-    ...Buttons.button,
+    padding: Spacing.smallest,
+    borderRadius: 50 / 2,
+    width: 50,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: Colors.red,
   },
   publishButtonText: {
     ...Buttons.buttonText,
     fontSize: 24,
     textAlign: 'center',
+  },
+  formField: {
+    ...Buttons.button,
+    ...Typography.bodyText,
+    padding: Spacing.small,
+    marginBottom: Spacing.base,
+    backgroundColor: Colors.gray900,
   },
 });
 
