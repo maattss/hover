0b5f584007d3c73d3c83ecdab9b8ee8fa8d0c15c
Siri Mykland <sirim_mykland@hotmail.com>
2021-02-05 13:35:27 +0100

    user_id is sent as route prop

--- types/navigationTypes.ts
+++ types/navigationTypes.ts
@@ -29,7 +29,7 @@ export type ChallengeStackParamList = {
   Challenge: undefined;
   PendingChallenges: PendingChallengesScreenProps;
   OngoingChallenges: OngoingChallengesScreenProps;
-  NewChallenge: undefined;
+  NewChallenge: { user_id: string };
   PickUsers: { user_id: string };
 };
 
