aee60a456d3906db2db00db4f4a6fd3e4602383b
Siri Mykland <sirim_mykland@hotmail.com>
2021-02-05 13:13:32 +0100

    added navigation for pick user screen

--- types/navigationTypes.ts
+++ types/navigationTypes.ts
@@ -30,6 +30,7 @@ export type ChallengeStackParamList = {
   PendingChallenges: PendingChallengesScreenProps;
   OngoingChallenges: OngoingChallengesScreenProps;
   NewChallenge: undefined;
+  PickUsers: { user_id: string };
 };
 
 export type NewChallengeStackParamList = {
