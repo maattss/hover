7f6f6a68394ddec05fa9c8ee04f21d13d14afc03
Mats Tyldum <ma.tyldum@gmail.com>
2021-02-05 15:53:03 +0100

    Update SignUpScreen.tsx

--- screens/auth/SignUpScreen.tsx
+++ screens/auth/SignUpScreen.tsx
@@ -1,43 +1,18 @@
 import React, { useState } from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
-import {
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Alert,
-  StyleSheet,
-  Keyboard,
-  Platform,
-  KeyboardAvoidingView,
-  ViewStyle,
-  Image,
-  Dimensions,
-} from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, Alert, StyleSheet } from 'react-native';
 import { RootStackParamList } from '../../types/navigationTypes';
 import Firebase, { fns } from '../../lib/firebase';
 import { Buttons, Colors, Spacing, Typography } from '../../theme';
 import Button from '../../components/Button';
 import Loading from '../../components/Loading';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Asset } from 'expo-asset';
 
 const SignUpScreen = ({ navigation }: StackScreenProps<RootStackParamList, 'Signup'>) => {
-  const insets = useSafeAreaInsets();
-  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-
   const [loading, setLoading] = useState(false);
   const [validationSuccess, setvalidationSuccess] = useState(true);
-
-  const randomPictureURI = () => {
-    const random = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    return 'https://api.multiavatar.com/' + random + '.png';
-  };
-  const [picture, setPicture] = useState(randomPictureURI());
 
   const handleSignup = async () => {
     setLoading(true);
@@ -50,8 +25,6 @@ const SignUpScreen = ({ navigation }: StackScreenProps<RootStackParamList, 'Sign
         await registerUser({ email, password });
         // Log the user in
         await Firebase.auth().signInWithEmailAndPassword(email, password);
-        // TODO:
-        // Add user to hasura db
         Alert.alert('Signup success');
       } else {
         Alert.alert('Something wrong...', 'Please check your email, and that both passwords match!');
@@ -68,17 +41,6 @@ const SignUpScreen = ({ navigation }: StackScreenProps<RootStackParamList, 'Sign
     setLoading(false);
   };
 
-  const getSafeAreaTop = () => {
-    return {
-      marginTop: insets.top,
-    } as ViewStyle;
-  };
-  const getSafeAreaHeight = () => {
-    return {
-      height: insets.top,
-    } as ViewStyle;
-  };
-
   if (loading) {
     return (
       <Loading text={'Signing up... Please wait'}>
@@ -90,65 +52,40 @@ const SignUpScreen = ({ navigation }: StackScreenProps<RootStackParamList, 'Sign
   } else {
     return (
       <View style={styles.container}>
-        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{ flex: 1 }}>
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View style={styles.inner}>
-              <View style={[styles.blackTop, getSafeAreaHeight()]} />
-              <View style={[styles.formContainer, getSafeAreaTop()]}>
-                <View style={styles.avatarContainer}>
-                  <Image
-                    // eslint-disable-next-line @typescript-eslint/no-var-requires
-                    loadingIndicatorSource={{ uri: Asset.fromModule(require('../../assets/images/user.png')).uri }}
-                    source={{ uri: picture }}
-                    style={styles.avatar}
-                  />
-                </View>
-                <Text style={styles.label}>Name</Text>
-                <TextInput
-                  placeholder="Enter your name"
-                  placeholderTextColor={Colors.gray600}
-                  onChangeText={(val) => setName(val)}
-                  style={styles.formField}
-                />
-                <Text style={styles.label}>E-mail</Text>
-                <TextInput
-                  placeholder="Enter your e-mail"
-                  placeholderTextColor={Colors.gray600}
-                  onChangeText={(val) => setEmail(val)}
-                  autoCapitalize="none"
-                  keyboardType="email-address"
-                  style={styles.formField}
-                />
-                <Text style={styles.label}>Password</Text>
-                <TextInput
-                  placeholder="Enter your password"
-                  placeholderTextColor={Colors.gray600}
-                  onChangeText={(val) => setPassword(val)}
-                  autoCapitalize="none"
-                  secureTextEntry
-                  autoCorrect={false}
-                  style={styles.formField}
-                />
-                <Text style={styles.label}>Confirm password</Text>
-                <TextInput
-                  placeholder="Confirm your password"
-                  placeholderTextColor={Colors.gray600}
-                  onChangeText={(val) => setConfirmPassword(val)}
-                  autoCapitalize="none"
-                  secureTextEntry
-                  style={styles.formField}
-                />
-                <Button onPress={handleSignup}>Sign up</Button>
-                <View style={styles.loginContainer}>
-                  <Text style={{ ...Typography.bodyText }}>Already have an account?</Text>
-                  <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-                    <Text style={styles.loginLink}>Login</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </View>
-          </TouchableWithoutFeedback>
-        </KeyboardAvoidingView>
+        <Text style={styles.header}>Sign up</Text>
+        <View style={styles.formContainer}>
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor={Colors.gray600}
+            onChangeText={(val) => setEmail(val)}
+            autoCapitalize="none"
+            keyboardType="email-address"
+            style={styles.formField}
+          />
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor={Colors.gray600}
+            onChangeText={(val) => setPassword(val)}
+            autoCapitalize="none"
+            secureTextEntry
+            style={styles.formField}
+          />
+          <TextInput
+            placeholder="Confirm password"
+            placeholderTextColor={Colors.gray600}
+            onChangeText={(val) => setConfirmPassword(val)}
+            autoCapitalize="none"
+            secureTextEntry
+            style={styles.formField}
+          />
+          <Button onPress={handleSignup}>Sign up</Button>
+        </View>
+        <View style={styles.loginContainer}>
+          <Text style={{ ...Typography.bodyText }}>Already have an account?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+            <Text style={styles.loginLink}>Login</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -156,46 +93,21 @@ const SignUpScreen = ({ navigation }: StackScreenProps<RootStackParamList, 'Sign
 
 const styles = StyleSheet.create({
   container: {
-    height: Dimensions.get('screen').height,
-  },
-  inner: {
-    justifyContent: 'flex-end',
+    display: 'flex',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    marginBottom: Spacing.base,
-  },
-  blackTop: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
+    height: '100%',
     width: '100%',
-    zIndex: 98,
-    backgroundColor: Colors.black,
+    marginTop: '30%',
   },
-  signUpContainer: {
-    alignItems: 'center',
+  header: {
+    ...Typography.headerText,
     marginBottom: Spacing.base,
-  },
-  label: {
-    ...Typography.bodyText,
-    fontWeight: 'bold',
-    marginBottom: Spacing.smallest,
     textAlign: 'left',
-    width: '85%',
+    width: '80%',
   },
   formContainer: {
-    width: '85%',
-  },
-  avatarContainer: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  avatar: {
-    height: 100,
-    width: 100,
-    borderRadius: 100 / 2,
-    margin: Spacing.small,
-    backgroundColor: Colors.white, // Default color
+    width: '80%',
   },
   formField: {
     ...Buttons.button,
@@ -220,12 +132,10 @@ const styles = StyleSheet.create({
     color: Colors.blue,
     marginLeft: Spacing.smallest,
     fontWeight: 'bold',
-    backgroundColor: 'green',
   },
   loginContainer: {
+    display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'center',
-    backgroundColor: 'red',
   },
 });
 
