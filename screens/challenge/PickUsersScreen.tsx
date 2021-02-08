4de307b53468768be2bea44478e3b37f2cef5162
Siri Mykland <sirim_mykland@hotmail.com>
2021-02-08 13:31:02 +0100

    pick users screen with props

--- screens/challenge/PickUsersScreen.tsx
+++ screens/challenge/PickUsersScreen.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
-import { Challenge_Participant_Insert_Input, Challenge_Participant_State_Enum } from '../../types/types';
 import { Avatar, CheckBox } from 'react-native-elements';
 import Loading from '../../components/general/Loading';
 import { NewChallengeStackParamList } from '../../types/navigationTypes';
@@ -9,56 +8,82 @@ import { RouteProp } from '@react-navigation/native';
 import { useGetFriendsQuery } from '../../graphql/queries/Friends.generated';
 import { ListUserFragmentFragment } from '../../graphql/Fragments.generated';
 import { Buttons, Colors, Spacing, Typography } from '../../theme';
+import Button from '../../components/general/Button';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { Challenge_Participant_Insert_Input } from '../../types/types';
 
 type PickUsersRouteProp = RouteProp<NewChallengeStackParamList, 'PickUsers'>;
+type NavigationProp = StackNavigationProp<NewChallengeStackParamList>;
 
 type Props = {
+  navigation: NavigationProp;
   route: PickUsersRouteProp;
 };
-const PickUsersScreen: React.FC<Props> = ({ route }: Props) => {
+const PickUsersScreen: React.FC<Props> = ({
+  route: {
+    params: { user_id, participants, setParticipants },
+  },
+  navigation: { goBack },
+}: Props) => {
   const { data: friends, loading } = useGetFriendsQuery({
-    variables: { user_id: route.params.user_id },
+    variables: { user_id: user_id },
   });
 
-  const participants: Challenge_Participant_Insert_Input[] = [
-    { user_id: route.params.user_id, state: Challenge_Participant_State_Enum.Accepted },
-  ];
+  const [localParticipants, setLocalParticipants] = useState<Challenge_Participant_Insert_Input[]>(participants);
 
   if (loading) return <Loading />;
 
   const onChecked = (id: string) => {
-    const index = participants.findIndex((x) => x.user_id == id);
+    const index = localParticipants.findIndex((x) => x.user_id == id);
+    const newParticipants = localParticipants;
     if (index > -1) {
-      participants.splice(index, 1);
+      newParticipants.splice(index, 1);
     } else {
-      participants.push({ user_id: id });
+      newParticipants.push({ user_id: id });
     }
-    console.log(participants);
+
+    setLocalParticipants(newParticipants);
+    console.log(newParticipants);
+  };
+
+  const isFriendChecked = (id: string) => {
+    if (localParticipants.findIndex((x) => x.user_id == id) > -1) return true;
+    return false;
   };
 
   const renderItem = (item: ListUserFragmentFragment) => {
     console.log(item.name);
-    return <FriendItem item={item} onValueChanged={() => onChecked(item.id)} />;
+    return <FriendItem item={item} checked={isFriendChecked(item.id)} onValueChanged={() => onChecked(item.id)} />;
   };
-
+  const renderSeparator = () => <View style={styles.seperator} />;
   return (
-    <View>
+    <View style={styles.container}>
       <FlatList
+        style={styles.box}
         data={friends?.users as ListUserFragmentFragment[]}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => renderItem(item)}
+        ItemSeparatorComponent={renderSeparator}
       />
+      <Button
+        onPress={() => {
+          setParticipants(localParticipants);
+          goBack();
+        }}>
+        Save Participants
+      </Button>
     </View>
   );
 };
 
 interface FriendItemProps {
   item: ListUserFragmentFragment;
+  checked: boolean;
   onValueChanged: (id: string) => void;
 }
 
 const FriendItem: React.FC<FriendItemProps> = (props: FriendItemProps) => {
-  const [checked, setChecked] = useState<boolean>(false);
+  const [checked, setChecked] = useState<boolean>(props.checked);
   const onPressed = () => {
     props.onValueChanged(props.item.id);
     setChecked(!checked);
@@ -76,7 +101,7 @@ const FriendItem: React.FC<FriendItemProps> = (props: FriendItemProps) => {
       <Avatar
         rounded
         source={{
-          uri: props.item.picture,
+          uri: props.item.picture ?? '',
         }}
       />
       <Text style={styles.label}>{props.item.name}</Text>
@@ -84,12 +109,29 @@ const FriendItem: React.FC<FriendItemProps> = (props: FriendItemProps) => {
   );
 };
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: Spacing.base,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  box: {
+    backgroundColor: Colors.gray900,
+    width: '100%',
+    borderRadius: Spacing.smaller,
+    paddingHorizontal: Spacing.base,
+    marginHorizontal: Spacing.smaller,
+    marginVertical: Spacing.smaller,
+  },
   checkboxContainer: {
     flexDirection: 'row',
     width: '100%',
     backgroundColor: Colors.gray900,
-    marginVertical: Spacing.smallest,
     alignItems: 'center',
+  },
+  seperator: {
+    backgroundColor: Colors.gray800,
+    height: 0.5,
   },
   checkbox: {
     alignSelf: 'center',
