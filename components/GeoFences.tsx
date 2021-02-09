50bef76c44749d708eb3dc704e5d26774cf25f4c
Mats Tyldum <ma.tyldum@gmail.com>
2021-02-09 18:27:51 +0100

    Added geofence icons to polygons

--- components/GeoFences.tsx
+++ components/GeoFences.tsx
@@ -7,29 +7,22 @@ interface GeoFencesProps {
   geofences?: GeoFence[];
 }
 
+const getOverlayBounds = (geoFence: GeoFence): [Coordinate, Coordinate] => {
+  return [
+    addMetersToLatLng(geoFence.latitude, geoFence.longitude, -geoFence.radius / 2, -geoFence.radius / 2),
+    addMetersToLatLng(geoFence.latitude, geoFence.longitude, geoFence.radius / 2, geoFence.radius / 2),
+  ];
+};
+
 const drawGeoFences = (geoFences: GeoFence[] | undefined) => {
   if (geoFences) {
     return geoFences.map((geoFence, index) => {
       if (geoFence.variant === GeoFenceVariant.CIRCLE) {
         const currentGeoFence = geoFence as CircleGeoFence;
-        const overLayBounds: [Coordinate, Coordinate] = [
-          addMetersToLatLng(
-            currentGeoFence.latitude,
-            currentGeoFence.longitude,
-            -currentGeoFence.radius / 2,
-            -currentGeoFence.radius / 2,
-          ),
-          addMetersToLatLng(
-            currentGeoFence.latitude,
-            currentGeoFence.longitude,
-            currentGeoFence.radius / 2,
-            currentGeoFence.radius / 2,
-          ),
-        ];
+        const overLayBounds: [Coordinate, Coordinate] = getOverlayBounds(currentGeoFence);
         return (
           <Fragment key={index}>
             <Circle
-              key={index}
               center={{ latitude: currentGeoFence.latitude, longitude: currentGeoFence.longitude }}
               radius={currentGeoFence.radius}
               fillColor={getGeoFenceColor(currentGeoFence.category, 0.6)}
@@ -41,14 +34,17 @@ const drawGeoFences = (geoFences: GeoFence[] | undefined) => {
         );
       } else if (geoFence.variant === GeoFenceVariant.POLYGON) {
         const currentGeoFence = geoFence as PolygonGeoFence;
+        const overLayBounds: [Coordinate, Coordinate] = getOverlayBounds(currentGeoFence);
         return (
-          <Polygon
-            key={index}
-            coordinates={currentGeoFence.coordinates}
-            fillColor={getGeoFenceColor(currentGeoFence.category, 0.6)}
-            strokeColor={getGeoFenceColor(currentGeoFence.category, 1)}
-            strokeWidth={1}
-          />
+          <Fragment key={index}>
+            <Polygon
+              coordinates={currentGeoFence.coordinates}
+              fillColor={getGeoFenceColor(currentGeoFence.category, 0.6)}
+              strokeColor={getGeoFenceColor(currentGeoFence.category, 1)}
+              strokeWidth={1}
+            />
+            <Overlay image={{ uri: getGeoFenceImage(currentGeoFence.category) }} bounds={overLayBounds} />
+          </Fragment>
         );
       }
     });
