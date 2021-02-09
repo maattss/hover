ea9d853eb37bc56185b76e7f7fec14ccbba555c0
Siri Mykland <sirim_mykland@hotmail.com>
2021-02-09 18:06:53 +0100

    participants, challengetype  rules are included in create challeneg flow

--- navigation/NewChallengeNavigator.tsx
+++ navigation/NewChallengeNavigator.tsx
@@ -5,6 +5,7 @@ import useAuthentication from '../hooks/useAuthentication';
 import NewChallengeOverviewScreen from '../screens/challenge/newchallenge/NewChallengeOverviewScreen';
 import PickUsersScreen from '../screens/challenge/newchallenge/PickUsersScreen';
 import ChallengeRulesScreen from '../screens/challenge/newchallenge/ChallengeRulesScreen';
+import ChallengeTypeScreen from '../screens/challenge/newchallenge/ChallengeTypeScreen';
 
 const NewChallengeStack = createStackNavigator<NewChallengeStackParamList>();
 
@@ -19,6 +20,13 @@ const NewChallengeNavigator: React.FC = () => {
           headerTitle: 'Pick Users',
         })}
         initialParams={{ user_id: user_id }}
+      />
+      <NewChallengeStack.Screen
+        name="ChallengeType"
+        component={ChallengeTypeScreen}
+        options={() => ({
+          headerTitle: 'Challenge type',
+        })}
       />
       <NewChallengeStack.Screen
         name="ChallengeRules"
