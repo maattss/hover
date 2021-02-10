79dbf50170f31c775e12f4cc05448de763bfaae4
Siri Mykland <sirim_mykland@hotmail.com>
2021-02-10 16:54:43 +0100

    overview lists mor infomationon challenge

--- screens/challenge/newchallenge/NewChallengeOverviewScreen.tsx
+++ screens/challenge/newchallenge/NewChallengeOverviewScreen.tsx
@@ -5,12 +5,15 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import Button from '../../../components/Button';
 import OpponentsList from '../../../components/challenge/OpponentsList';
+import { FontAwesome5 as FA5Icon } from '@expo/vector-icons';
 import Separator from '../../../components/Separator';
 import { useInsertChallengeMutation } from '../../../graphql/mutations/InsertChallenge.generated';
-import { convertToJsonRule, getChallengeTypeEnum } from '../../../helpers/challengeMappers';
+import { convertToJsonRule } from '../../../helpers/challengeMappers';
 import { Buttons, Colors, Spacing, Typography } from '../../../theme';
 import { NewChallengeStackParamList, RootTabParamList } from '../../../types/navigationTypes';
 import { Challenge_Participant_Insert_Input, Challenge_Participant_State_Enum } from '../../../types/types';
+import { GeoFenceCategory } from '../../../types/geoFenceTypes';
+import { getCategoryColor, getCategoryIconName } from '../../../helpers/categoryHelpers';
 
 type NewChallengeRouteProp = RouteProp<NewChallengeStackParamList, 'NewChallengeOverview'>;
 type NavigationProp = StackNavigationProp<RootTabParamList, 'Challenge'>;
@@ -37,24 +40,53 @@ const NewChallengeOverviewScreen: React.FC<Props> = ({ route, navigation }: Prop
   };
 
   const [createChallenge] = useInsertChallengeMutation();
-
+  const categoryColor = {
+    color: getCategoryColor(GeoFenceCategory[route.params.rules?.category as keyof typeof GeoFenceCategory]),
+  };
   return (
     <View style={styles.container}>
       <View style={styles.box}>
         <Text style={{ ...Typography.headerText }}>Overview</Text>
 
-        <View style={styles.box}>
+        <View style={styles.infoContainer}>
+          <Text style={styles.infoText}>Against</Text>
           <OpponentsList opponents={route.params.participants} />
         </View>
-
         <Separator />
+        {route.params.rules?.score && (
+          <View style={styles.infoContainer}>
+            <Text style={styles.infoText}>Goal score</Text>
+            <Text style={styles.infoTextSmall}>{route.params.rules?.score}</Text>
+          </View>
+        )}
+        {route.params.rules?.time && (
+          <View style={styles.infoContainer}>
+            <Text style={styles.infoText}>Goal time</Text>
+            <Text style={styles.infoTextSmall}>{route.params.rules?.time} hours</Text>
+          </View>
+        )}
+        {route.params.rules?.category && (
+          <View style={[styles.infoContainer, { marginBottom: 0 }]}>
+            <Text style={styles.infoText}>Category</Text>
+            <FA5Icon
+              style={[styles.categoryIcon, categoryColor]}
+              name={getCategoryIconName(
+                GeoFenceCategory[route.params.rules?.category as keyof typeof GeoFenceCategory],
+              )}
+            />
+          </View>
+        )}
+        <View style={styles.infoContainer}>
+          <Text style={styles.infoText}>Before</Text>
+          <Text style={styles.infoTextSmall}>{route.params.end_date.toDateString()}</Text>
+        </View>
       </View>
       <Button
         onPress={() =>
           createChallenge({
             variables: {
-              challenge_type: getChallengeTypeEnum(route.params.challenge_type.name),
-              end_date: route.params.end_date,
+              challenge_type: route.params.challenge_type,
+              end_date: route.params.end_date.toDateString(),
               participants: toParticipantList(),
               created_by: route.params.user_id,
               rules: convertToJsonRule(route.params.rules),
@@ -88,11 +120,37 @@ const styles = StyleSheet.create({
     width: '100%',
     left: '20%',
   },
-  opponentHeaderText: {
+  middleHeaderText: {
     color: Colors.almostWhite,
     fontSize: 12,
     fontWeight: 'bold',
     paddingVertical: Spacing.base,
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: Spacing.base,
+    alignItems: 'center',
+  },
+  infoText: {
+    ...Typography.largeBodyText,
+    fontSize: 26,
+    fontWeight: 'bold',
+  },
+  infoTextSmall: {
+    ...Typography.largeBodyText,
+    fontSize: 18,
+  },
+  infoScore: {
+    ...Typography.headerText,
+    marginBottom: Spacing.small,
+    textAlign: 'center',
+  },
+  categoryIcon: {
+    color: Colors.almostWhite,
+    fontSize: 42,
+    textAlign: 'center',
+    paddingRight: Spacing.smallest,
   },
   challengeButton: {
     ...Buttons.button,
