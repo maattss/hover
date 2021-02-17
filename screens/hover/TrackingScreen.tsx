85d4149f05adfc4a101311db58eac5886a51e80f
Mats Tyldum <ma.tyldum@gmail.com>
2021-02-17 17:46:04 +0100

    Added unique word generator

--- screens/hover/TrackingScreen.tsx
+++ screens/hover/TrackingScreen.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator, TextInput } from 'react-native';
+import { uniqueNamesGenerator, Config, adjectives, animals } from 'unique-names-generator';
 import { Colors, Spacing, Typography, Buttons } from '../../theme';
 import useTracking from '../../hooks/useTracking';
 import HoverMap from '../../components/map/HoverMap';
 
 const TrackingScreen: React.FC = () => {
   const tracking = useTracking();
+  const [collabCode, setCollabCode] = useState('');
   const stopTracking = () => tracking.pauseTracking();
+
+  const wordConfig: Config = {
+    dictionaries: [adjectives, animals],
+    separator: ' ',
+    style: 'upperCase',
+    length: 2,
+  };
+
+  const collabName: string = uniqueNamesGenerator(wordConfig);
 
   return (
     <View style={styles.container}>
@@ -14,20 +25,30 @@ const TrackingScreen: React.FC = () => {
 
       <View style={styles.trackingContainer}>
         <View style={styles.collabInfo}>
-          <Text style={{ ...Typography.headerText, padding: 20 }}>hey</Text>
+          <Text style={{ ...Typography.headerText }}>Hover with friends</Text>
+          <Text style={{ ...Typography.largeBodyText }}>Your code: {collabName}</Text>
+          <TextInput
+            placeholder="Enter code"
+            placeholderTextColor={Colors.gray600}
+            onChangeText={(val) => setCollabCode(val)}
+            style={styles.formField}
+          />
         </View>
 
         <View style={styles.trackingInfo}>
-          <View>
-            <ActivityIndicator size={'large'} color={Colors.blue} />
-            <Text style={styles.scoreText}>Points: {Math.floor(tracking.score)}</Text>
-          </View>
+          <Text style={{ ...Typography.headerText }}>Tracking info</Text>
+          <View style>
+            <View>
+              <ActivityIndicator size={'large'} color={Colors.blue} />
+              <Text style={styles.scoreText}>Points: {Math.floor(tracking.score)}</Text>
+            </View>
 
-          <View style={styles.stopButtonContainer}>
-            <TouchableOpacity style={styles.stopButton} onPress={stopTracking}>
-              <Text style={styles.stopButtonText}>Stop</Text>
-            </TouchableOpacity>
-            <View />
+            <View style={styles.stopButtonContainer}>
+              <TouchableOpacity style={styles.stopButton} onPress={stopTracking}>
+                <Text style={styles.stopButtonText}>Stop</Text>
+              </TouchableOpacity>
+              <View />
+            </View>
           </View>
         </View>
       </View>
@@ -42,31 +63,33 @@ const styles = StyleSheet.create({
   trackingContainer: {
     position: 'absolute',
     bottom: 0,
-    left: 0,
-    margin: Spacing.smallest,
-    backgroundColor: Colors.almostBlackTransparent,
-    borderRadius: Spacing.smaller,
-    width: '100%',
+    left: '1%',
+    width: '98%',
+    backgroundColor: 'red',
+    marginBottom: Spacing.smaller,
   },
   collabInfo: {
     width: '100%',
-    flexDirection: 'row',
     justifyContent: 'center',
+    backgroundColor: Colors.almostBlackTransparent,
+    borderRadius: Spacing.smaller,
+    padding: Spacing.smaller,
     // backgroundColor: Colors.redTransparent,
   },
   trackingInfo: {
     alignItems: 'center',
     justifyContent: 'space-evenly',
-    paddingHorizontal: Spacing.smaller,
+    padding: Spacing.smaller,
+    marginTop: Spacing.smaller,
     flexDirection: 'row',
-    // backgroundColor: Colors.bluTransparent,
+    backgroundColor: Colors.almostBlackTransparent,
+    borderRadius: Spacing.smaller,
   },
   stopButtonContainer: {
     justifyContent: 'center',
     paddingHorizontal: Spacing.smaller,
   },
   stopButton: {
-    padding: Spacing.smallest,
     borderRadius: 110 / 2,
     width: 110,
     height: 110,
@@ -86,6 +109,13 @@ const styles = StyleSheet.create({
     ...Typography.headerText,
     marginTop: Spacing.base,
     marginBottom: Spacing.base,
+  },
+  formField: {
+    ...Buttons.button,
+    ...Typography.bodyText,
+    padding: Spacing.base,
+    marginBottom: Spacing.base,
+    backgroundColor: Colors.gray900,
   },
 });
 
