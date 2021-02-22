51f6b9096ccec0b6c571c240f42f6d84c7856500
Siri Mykland <sirim_mykland@hotmail.com>
2021-02-22 17:14:41 +0100

    challenge feed card component

--- components/feed/ChallengeFeedCard.tsx
+++ components/feed/ChallengeFeedCard.tsx
@@ -1,36 +1,102 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Colors, Typography, Spacing } from '../../theme';
 import { ChallengeFeedData } from '../../types/feedTypes';
 import { timeStampToPresentable } from '../../helpers/dateTimeHelpers';
 import { defaultUserProfile } from '../../helpers/objectMappers';
-import { generateOngoingChallengeDescription } from '../../helpers/decriptionHelper';
+import { generateFeedChallengeDescription } from '../../helpers/decriptionHelper';
+import { Avatar } from 'react-native-elements';
+import Leaderboard, { Item } from '../leaderboard/Leaderboard';
+import { FontAwesome5 as FAIcon } from '@expo/vector-icons';
+import { getAchievementColor } from '../profile/Achievement';
+import Divider from '../general/Divider';
+import ProfileButton from '../ProfileButton';
 
-interface ChallengeFeedCardProps {
+type ChallengeFeedCardProps = {
   data: ChallengeFeedData;
-}
+};
 
 const ChallengeFeedCard: React.FC<ChallengeFeedCardProps> = ({ data }: ChallengeFeedCardProps) => {
+  const renderItem = (item: Item, index: number) => {
+    const rowJSx = (
+      <View key={item.id}>
+        <ChallengeLeaderboardRow item={item} index={index} />
+      </View>
+    );
+    return <ProfileButton user_id={item.id}>{rowJSx}</ProfileButton>;
+  };
+
   return (
     <View style={styles.card}>
+      <View style={[styles.topBar, { padding: Spacing.smaller }]}>
+        <Text style={{ ...Typography.headerText }}>We have a winner... </Text>
+      </View>
       <View style={styles.topBar}>
-        <Image
-          source={{ uri: data.user.picture ? data.user.picture : defaultUserProfile.picture }}
-          style={styles.avatar}
-        />
-        <Text style={styles.nameText}>{data.user.name}</Text>
-      </View>
-
-      <View style={styles.main}>
-        <View style={styles.description}>
-          <Text style={styles.descriptionText}>{generateOngoingChallengeDescription(data.challenge)}</Text>
+        <View style={styles.avatar}>
+          <Avatar
+            rounded
+            source={{ uri: data.user.picture ? data.user.picture : defaultUserProfile.picture }}
+            size={'medium'}
+          />
         </View>
-        <View style={styles.challenge}></View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.nameText}>{data.user.name}</Text>
+          <Text style={styles.descriptionText}>{generateFeedChallengeDescription(data.challenge)}</Text>
+        </View>
       </View>
-
+      <Divider />
+      <View style={styles.main}>
+        <Leaderboard
+          data={data.challenge.opponents.map<Item>((item) => {
+            return { id: item.user.id, name: item.user.name, picture: item.user.picture, score: item.progress } as Item;
+          })}
+          renderItem={renderItem}
+          seperator={() => <Divider style={{ borderBottomColor: Colors.gray800 }} />}
+        />
+      </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>{timeStampToPresentable(data.createdAt)}</Text>
       </View>
+    </View>
+  );
+};
+
+const ChallengeLeaderboardRow = ({ item, index }: { item: Item; index: number }) => {
+  const iconColor = {
+    color: getAchievementColor(index + 1),
+  };
+  const textSize =
+    index == 0
+      ? {
+          fontSize: 28,
+        }
+      : {
+          fontSize: 20,
+        };
+  const rowHeight =
+    index == 0
+      ? {
+          height: 60,
+        }
+      : {
+          height: 35,
+        };
+  return (
+    <View style={[styles.row, rowHeight]}>
+      <View style={styles.left}>
+        <Text style={[styles.text, styles.rank, index < 9 ? styles.singleDidget : styles.doubleDidget]}>
+          {index < 3 ? <FAIcon name={'medal'} style={[iconColor, textSize]} /> : index + 1}
+        </Text>
+        {item.picture && (
+          <View style={styles.avatar}>
+            <Avatar rounded source={{ uri: item.picture ? item.picture : defaultUserProfile.picture }} size={'small'} />
+          </View>
+        )}
+        <Text style={[styles.text, styles.label, textSize]} numberOfLines={1}>
+          {item.name}
+        </Text>
+      </View>
+      <Text style={[styles.text, styles.score, textSize]}>{item.score || 0}</Text>
     </View>
   );
 };
@@ -45,30 +111,30 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-start',
   },
+  avatar: {
+    marginRight: Spacing.small,
+    alignSelf: 'center',
+  },
+  infoContainer: {
+    padding: Spacing.smaller,
+    justifyContent: 'center',
+    flexShrink: 1,
+  },
   nameText: {
     ...Typography.headerText,
     fontSize: 20,
-    lineHeight: 50,
-  },
-  description: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    width: '65%',
   },
   descriptionText: {
-    ...Typography.headerText,
-    fontSize: 24,
+    color: Colors.almostWhite,
+    fontSize: 12,
+    fontStyle: 'italic',
     flexWrap: 'wrap',
-    flexDirection: 'column',
-    maxWidth: '100%',
-    paddingLeft: Spacing.smaller,
   },
   main: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: Spacing.smaller,
-    width: '100%',
+    borderRadius: Spacing.smaller,
+    backgroundColor: Colors.almostBlack,
+    paddingVertical: Spacing.small,
+    marginBottom: Spacing.small,
   },
   footer: {
     flexDirection: 'row',
@@ -79,15 +145,40 @@ const styles = StyleSheet.create({
     fontStyle: 'italic',
     fontSize: 14,
   },
-  avatar: {
-    height: 45,
-    width: 45,
-    borderRadius: 45 / 2,
-    marginRight: Spacing.small,
+  row: {
+    paddingVertical: Spacing.smallest,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
-  challenge: {
-    marginTop: -40,
-    width: '35%',
+  left: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  rank: {
+    fontWeight: 'bold',
+    marginRight: 5,
+    width: '15%',
+  },
+  singleDidget: {
+    paddingLeft: Spacing.base,
+    paddingRight: 6,
+  },
+  doubleDidget: {
+    paddingLeft: 10,
+    paddingRight: 2,
+  },
+  label: {
+    flex: 1,
+  },
+  score: {
+    fontWeight: 'bold',
+    position: 'absolute',
+    right: Spacing.base,
+    paddingLeft: Spacing.base,
+  },
+  text: {
+    ...Typography.bodyText,
   },
 });
 
