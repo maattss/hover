dc739cfc5ac2bba94dd3516f5f2d11347cb66206
Siri Mykland <sirim_mykland@hotmail.com>
2021-02-25 16:19:50 +0100

    added notification card

---
+++ components/feed/notification/NotificationCard.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import { View, Text, StyleSheet, Alert } from 'react-native';
+import { FontAwesome5 as FAIcon } from '@expo/vector-icons';
+import { Colors, Spacing, Typography } from '../../../theme';
+import { NotificationFragmentFragment } from '../../../graphql/Fragments.generated';
+import { getNotificationTitle } from '../../../helpers/notificationHelpers';
+import { timeStampToPresentable } from '../../../helpers/dateTimeHelpers';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { RootTabParamList } from '../../../types/navigationTypes';
+import { useNavigation } from '@react-navigation/native';
+import { Notification_Type_Enum } from '../../../types/types';
+
+type BottomNavigationProp = BottomTabNavigationProp<RootTabParamList>;
+
+interface NotificationCardProps {
+  notification: NotificationFragmentFragment;
+}
+const NotificationCard: React.FC<NotificationCardProps> = (props: NotificationCardProps) => {
+  const bgColor = props.notification.seen ? Colors.almostBlack : Colors.gray900;
+  const bottomNavigation = useNavigation<BottomNavigationProp>();
+
+  const getNotificationGoToAction = () => {
+    switch (props.notification.type) {
+      case Notification_Type_Enum.ChallengeClosed:
+        bottomNavigation.navigate('Challenge');
+        break;
+      case Notification_Type_Enum.ChallengeExpired:
+        Alert.alert('This object does not have an action');
+        break;
+      case Notification_Type_Enum.ChallengeFinished:
+        bottomNavigation.navigate('Feed');
+        break;
+      case Notification_Type_Enum.ChallengeInvite:
+        bottomNavigation.navigate('Challenge');
+        break;
+      case Notification_Type_Enum.ChallengeWon:
+        bottomNavigation.navigate('Feed');
+        break;
+      case Notification_Type_Enum.NewAchievement:
+        bottomNavigation.navigate('Profile');
+        break;
+      case Notification_Type_Enum.ParticipantUpdate:
+        bottomNavigation.navigate('Challenge');
+        break;
+      default:
+        Alert.alert('This object does not have an action');
+        break;
+    }
+  };
+  return (
+    <TouchableOpacity onPress={() => getNotificationGoToAction()}>
+      <View style={[styles.card, { backgroundColor: bgColor }]}>
+        <Text style={styles.title}>{getNotificationTitle(props.notification.type)}</Text>
+        <View style={styles.main}>
+          <View style={styles.body}>
+            <Text style={{ ...Typography.bodyText }}>{props.notification.text}</Text>
+          </View>
+          <View style={styles.goTo}>
+            <FAIcon name={'chevron-right'} style={{ ...Typography.bodyText }} />
+          </View>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>{timeStampToPresentable(props.notification.created_at)}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default NotificationCard;
+
+const styles = StyleSheet.create({
+  card: {
+    paddingHorizontal: Spacing.base,
+    paddingVertical: Spacing.smaller,
+  },
+  title: {
+    ...Typography.headerText,
+    fontSize: 20,
+  },
+  main: {
+    ...Typography.bodyText,
+    marginVertical: Spacing.smallest,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  body: {
+    width: '90%',
+    paddingRight: Spacing.small,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  goTo: {
+    width: '10%',
+    paddingLeft: Spacing.small,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    width: '100%',
+  },
+  footerText: {
+    color: Colors.almostWhite,
+    fontStyle: 'italic',
+    fontSize: 14,
+  },
+});
--- screens/feed/NotificationsScreen.tsx
+++ screens/feed/NotificationsScreen.tsx
@@ -5,11 +5,76 @@ import Error from '../../components/general/Error';
 import Loading from '../../components/general/Loading';
 import { NotificationFragmentFragment } from '../../graphql/Fragments.generated';
 import { useNotifiactionsQuery } from '../../graphql/queries/Notifications.generated';
-import { getNotificationTitle } from '../../helpers/notificationHelpers';
+import NotificationCard from '../../components/feed/notification/NotificationCard';
 
 const NotificationsScreen: React.FC = () => {
-  const [notifications, setNotification] = useState<NotificationFragmentFragment[]>([]);
+  const allData = {
+    data: {
+      notifications: [
+        {
+          id: 3,
+          type: 'CHALLENGE_CLOSED',
+          text:
+            'Your score in category challenge have been closed due to insuficient  number of participants. This may be because some participants have declined your challenge.',
+          seen: false,
+          user_id: 'klvKqLVwByQbnFiEID8gjtHlvjD3',
+          created_at: '2021-02-25T12:17:40.496442+00:00',
+        },
+        {
+          id: 2,
+          type: 'CHALLENGE_EXPIRED',
+          text: 'Your challenge expired.',
+          seen: false,
+          user_id: 't1Kg1gYha8hwez4Wzqs1Kvr9FXf2',
+          created_at: '2021-02-25T12:17:40.496442+00:00',
+        },
+        {
+          id: 90,
+          type: 'CHALLENGE_FINISHED',
+          text: 'Ronny invited you to a time in category challenge.',
+          seen: false,
+          user_id: 'uqoRoWbmv7P457uvGk9IbX8sH143',
+          created_at: '2021-02-25T12:17:40.496442+00:00',
+        },
+        {
+          id: 9,
+          type: 'CHALLENGE_INVITE',
+          text: 'Ronny invited you to a time in category challenge.',
+          seen: false,
+          user_id: 'uqoRoWbmv7P457uvGk9IbX8sH143',
+          created_at: '2021-02-25T12:17:40.496442+00:00',
+        },
+        {
+          id: 14,
+          type: 'CHALLENGE_WON',
+          text: 'You won a challenge',
+          seen: true,
+          user_id: 'Srnquo5AgrfB1S8FD6PvdhoZLwe2',
+          created_at: '2021-02-25T12:17:40.496442+00:00',
+        },
+        {
+          id: 17,
+          type: 'NEW_ACHIEVEMENT',
+          text: 'You achieved a new achievement! Check it out in the feed or your profile page!',
+          seen: false,
+          user_id: 'UV8a00JBWUh3XAilTq60MElESfy2',
+          created_at: '2021-02-25T14:26:19.036395+00:00',
+        },
+        {
+          id: 1,
+          type: 'PARTICIPANT_UPDATE',
+          text: 'Siri Mykland accepted your time challenge.',
+          seen: false,
+          user_id: 't1Kg1gYha8hwez4Wzqs1Kvr9FXf2',
+          created_at: '2021-02-25T12:17:40.496442+00:00',
+        },
+      ],
+    },
+  };
 
+  const [notifications, setNotification] = useState<NotificationFragmentFragment[]>(
+    allData.data.notifications as NotificationFragmentFragment[],
+  );
   const limit = 5;
   const [offset, setOffset] = useState(0);
   const [endReached, setEndReached] = useState(false);
@@ -28,7 +93,7 @@ const NotificationsScreen: React.FC = () => {
         data.notifications.forEach((obj: NotificationFragmentFragment) => {
           newNotification.push(obj);
         });
-        setNotification(newNotification);
+        //setNotification(newNotification);
       }
     }
   }, [data]);
@@ -51,11 +116,7 @@ const NotificationsScreen: React.FC = () => {
       <Text style={{ ...Typography.headerText, marginTop: Spacing.base }}>Notifications</Text>
     </View>
   );
-  const renderItem = (item: NotificationFragmentFragment) => (
-    <View style={{ marginHorizontal: Spacing.smaller }}>
-      <Text style={{ ...Typography.bodyText }}>{getNotificationTitle(item.type)}</Text>
-    </View>
-  );
+  const renderItem = (item: NotificationFragmentFragment) => <NotificationCard notification={item} />;
   const renderFooter = () => {
     return (
       <View style={styles.footer}>
