ed0b9ede97d8e7272386b0bb44c4211d9136aff2
Mats Tyldum <ma.tyldum@gmail.com>
2021-02-26 09:48:35 +0100

    Fixed achievement name

--- components/feed/AchievementFeedCard.tsx
+++ components/feed/AchievementFeedCard.tsx
@@ -14,19 +14,24 @@ interface AchievementFeedCardProps {
 const AchievementFeedCard: React.FC<AchievementFeedCardProps> = ({ data }: AchievementFeedCardProps) => {
   return (
     <View style={styles.card}>
-      <TouchableProfile user_id={data.user.id} name={data.user.name}>
-        <View style={styles.topBar}>
-          <Image
-            source={{ uri: data.user.picture ? data.user.picture : defaultUserProfile.picture }}
-            style={styles.avatar}
-          />
-          <Text style={styles.nameText}>{data.user.name}</Text>
-        </View>
-      </TouchableProfile>
       <View style={styles.main}>
-        <View style={styles.description}>
-          <Text style={styles.descriptionText}>{data.achievement.description}</Text>
+        <View style={styles.infoContainer}>
+          <TouchableProfile user_id={data.user.id} name={data.user.name}>
+            <View style={styles.topBar}>
+              <Image
+                source={{ uri: data.user.picture ? data.user.picture : defaultUserProfile.picture }}
+                style={styles.avatar}
+              />
+              <Text style={styles.nameText} numberOfLines={1}>
+                {data.user.name}
+              </Text>
+            </View>
+          </TouchableProfile>
+          <View style={styles.description}>
+            <Text style={styles.descriptionText}>{data.achievement.description}</Text>
+          </View>
         </View>
+
         <View style={styles.achievement}>
           <Achievement achievement={data.achievement} />
         </View>
@@ -43,36 +48,37 @@ const styles = StyleSheet.create({
   card: {
     backgroundColor: Colors.gray900,
     borderRadius: Spacing.smaller,
-    padding: Spacing.small,
+    padding: Spacing.smallest,
   },
   topBar: {
     flexDirection: 'row',
     justifyContent: 'flex-start',
   },
+  infoContainer: {
+    width: '55%',
+  },
   nameText: {
     ...Typography.headerText,
     fontSize: 20,
     lineHeight: 50,
+    flexWrap: 'wrap',
   },
   description: {
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    width: '65%',
   },
   descriptionText: {
     ...Typography.headerText,
     fontSize: 24,
     flexWrap: 'wrap',
     flexDirection: 'column',
-    maxWidth: '100%',
     paddingLeft: Spacing.smaller,
   },
   main: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginBottom: Spacing.smaller,
-    width: '100%',
   },
   footer: {
     flexDirection: 'row',
@@ -90,7 +96,6 @@ const styles = StyleSheet.create({
     marginRight: Spacing.small,
   },
   achievement: {
-    marginTop: -40,
     width: '35%',
   },
 });
