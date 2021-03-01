9968c182d205554cc0a17ccb6d1d5a42419ff1b3
Mats Tyldum <ma.tyldum@gmail.com>
2021-03-01 13:59:48 +0100

    Update pushNotifications.ts

--- helpers/pushNotifications.ts
+++ helpers/pushNotifications.ts
@@ -2,13 +2,13 @@ import Constants from 'expo-constants';
 import * as Notifications from 'expo-notifications';
 import { Platform, Alert } from 'react-native';
 
-export const sendPushNotification = async (expoPushToken: string) => {
+export const sendPushNotification = async (expoPushToken: string, title: string, body: string, sound: boolean) => {
+  // Message fields: https://docs.expo.io/push-notifications/sending-notifications/#message-request-format
   const message = {
     to: expoPushToken,
-    sound: 'default',
-    title: 'Original Title',
-    body: 'And here is the body!',
-    data: { someData: 'goes here' },
+    sound: sound ? 'default' : null,
+    title: title,
+    body: body,
   };
 
   await fetch('https://exp.host/--/api/v2/push/send', {
