94220e0c00a4b34fc468772d868aa6d91c588ef6
Mats Tyldum <ma.tyldum@gmail.com>
2021-03-01 10:26:54 +0100

    New publish screen

--- screens/hover/PublishScreen.tsx
+++ screens/hover/PublishScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Alert, Image } from 'react-native';
-import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { StyleSheet, Text, View, Alert, Image, ViewStyle } from 'react-native';
+import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import useTracking from '../../hooks/useTracking';
 import { Buttons, Colors, Spacing, Typography } from '../../theme';
 import { FontAwesome as FAIcon } from '@expo/vector-icons';
@@ -8,9 +8,14 @@ import { durationToTimestamp, timeStampToHours } from '../../helpers/dateTimeHel
 import Button from '../../components/general/Button';
 import { getGeoFenceImage } from '../../helpers/geoFenceCalculations';
 import KeyboardAvoiderNoHeader from '../../components/keyboard/KeyboardAvoiderNoHeader';
+import HoverMap from '../../components/map/HoverMap';
+import MapView, { LatLng, Marker, Region } from 'react-native-maps';
+import GeoFences from '../../components/map/GeoFences';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const PublishScreen: React.FC = () => {
   const tracking = useTracking();
+  const insets = useSafeAreaInsets();
   const [caption, setCaption] = useState('');
 
   const publishActivity = () => tracking.stopTracking(caption);
@@ -25,11 +30,48 @@ const PublishScreen: React.FC = () => {
       },
     ]);
   };
+  const safeTop = () => {
+    return {
+      marginTop: insets.top,
+    } as ViewStyle;
+  };
+
+  const renderMap = () => {
+    if (tracking.insideGeoFence) {
+      const mapRegion: Region = {
+        latitude: tracking.insideGeoFence?.latitude,
+        longitude: tracking.insideGeoFence?.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      };
+      const markerCoordinate: LatLng = {
+        latitude: tracking.insideGeoFence?.latitude,
+        longitude: tracking.insideGeoFence?.longitude,
+      };
+      return (
+        <MapView
+          style={styles.map}
+          initialRegion={mapRegion}
+          rotateEnabled={false}
+          scrollEnabled={false}
+          pitchEnabled={false}
+          zoomEnabled={false}>
+          <Marker
+            coordinate={markerCoordinate}
+            title={tracking.insideGeoFence?.name ?? 'No name'}
+            description={tracking.insideGeoFence?.description ?? 'No description'}
+          />
+          <GeoFences geofences={[tracking.insideGeoFence]} />
+        </MapView>
+      );
+    }
+    return <></>;
+  };
 
   return (
-    <KeyboardAvoiderNoHeader>
-      <View style={styles.container}>
-        <View style={styles.topBar}>
+    <View style={safeTop()}>
+      <ScrollView contentInset={{ bottom: 100 }} style={{ padding: Spacing.smaller }}>
+        <View style={[styles.topBar]}>
           <View style={styles.topBarIcon}>
             <FAIcon name={'question-circle'} style={styles.questionIcon} />
           </View>
@@ -51,49 +93,98 @@ const PublishScreen: React.FC = () => {
 
         <View style={styles.summaryContainer}>
           <Text style={styles.infoScore}>{Math.floor(tracking.score)} points</Text>
+
           <View style={styles.infoContainer}>
-            <Text style={styles.infoText}>Duration</Text>
-            <Text style={styles.infoTextSmall}>{durationToTimestamp(tracking.duration)}</Text>
-          </View>
-          <View style={styles.infoContainer}>
-            <Text style={styles.infoText}>Started at</Text>
-            <Text style={styles.infoTextSmall}>{timeStampToHours(tracking.trackingStart)}</Text>
-          </View>
-          <View style={styles.infoContainer}>
-            <Text style={styles.infoText}>Location</Text>
-            <Text style={styles.infoTextSmall}>{tracking.insideGeoFence?.name}</Text>
-          </View>
-          <View style={[styles.infoContainer, { marginBottom: 0 }]}>
-            <Text style={styles.infoText}>Category</Text>
-            <Image source={{ uri: getGeoFenceImage(tracking.insideGeoFence?.category) }} style={styles.categoryIcon} />
+            <View style={{ width: '49%' }}>
+              <View
+                style={{
+                  backgroundColor: Colors.gray900,
+                  borderRadius: Spacing.smaller,
+                  padding: Spacing.smaller,
+                  height: '100%',
+                  justifyContent: 'center',
+                }}>
+                <View style={{ marginBottom: Spacing.smaller }}>
+                  <Text style={styles.infoText}>Duration</Text>
+                  <Text style={styles.infoTextSmall}>{durationToTimestamp(tracking.duration)}</Text>
+                </View>
+                <View style={{ marginBottom: Spacing.smaller }}>
+                  <Text style={styles.infoText}>Started at</Text>
+                  <Text style={styles.infoTextSmall}>{timeStampToHours(tracking.trackingStart)}</Text>
+                </View>
+                {tracking.insideGeoFence && (
+                  <View style={{ marginBottom: Spacing.smaller }}>
+                    <Text style={styles.infoText}>Location</Text>
+                    <Text style={styles.infoTextSmall}>{tracking.insideGeoFence?.name}</Text>
+                  </View>
+                )}
+              </View>
+            </View>
+            <View style={{ width: '49%' }}>
+              <View
+                style={{
+                  backgroundColor: Colors.gray900,
+                  borderRadius: Spacing.smaller,
+                  padding: Spacing.smallest,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '100%',
+                }}>
+                <Image
+                  source={{ uri: getGeoFenceImage(tracking.insideGeoFence?.category) }}
+                  style={styles.categoryIcon}
+                />
+                <Text style={{ ...Typography.largeBodyText }}>{tracking.insideGeoFence?.category}</Text>
+              </View>
+            </View>
           </View>
 
-          <TextInput
-            placeholder="Insert a funny text that describes the activity!"
-            placeholderTextColor={Colors.gray600}
-            onChangeText={(val) => setCaption(val)}
-            style={styles.formField}
-            multiline>
-            {caption}
-          </TextInput>
+          {tracking.insideGeoFence && (
+            <View style={{ marginBottom: Spacing.base }}>
+              <Text style={styles.label}>Map</Text>
+              {renderMap()}
+            </View>
+          )}
+
+          <View>
+            <Text style={styles.label}>Caption</Text>
+            <TextInput
+              placeholder="Insert a funny text that describes the activity!"
+              placeholderTextColor={Colors.gray600}
+              onChangeText={(val) => setCaption(val)}
+              style={styles.formField}
+              multiline>
+              {caption}
+            </TextInput>
+          </View>
         </View>
+      </ScrollView>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: Spacing.smaller,
+          left: Spacing.smaller,
+          right: Spacing.smaller,
+        }}>
         <Button onPress={publishActivity}>
           <Text style={styles.publishButtonText}>Publish</Text>
         </Button>
       </View>
-    </KeyboardAvoiderNoHeader>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    padding: Spacing.smaller,
+  label: {
+    ...Typography.largeBodyText,
+    fontWeight: 'bold',
+    marginBottom: Spacing.smallest,
+    marginLeft: Spacing.smallest,
   },
   categoryIcon: {
-    height: 50,
-    width: 50,
-    marginVertical: Spacing.smallest,
-    paddingRight: Spacing.smallest,
+    height: '50%',
+    width: '50%',
+    marginBottom: Spacing.small,
   },
   summaryContainer: {
     marginTop: Spacing.base,
@@ -103,26 +194,25 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     marginBottom: Spacing.base,
     alignItems: 'center',
+    height: '35%',
   },
   infoText: {
     ...Typography.largeBodyText,
-    fontSize: 26,
     fontWeight: 'bold',
   },
   infoTextSmall: {
     ...Typography.largeBodyText,
-    fontSize: 18,
+    fontSize: 16,
   },
   infoScore: {
     ...Typography.headerText,
-    marginBottom: Spacing.small,
+    marginBottom: Spacing.base,
     textAlign: 'center',
   },
   formField: {
     ...Buttons.button,
     ...Typography.bodyText,
     paddingTop: Spacing.small,
-    marginVertical: Spacing.smaller,
     backgroundColor: Colors.gray900,
   },
   publishButton: {
@@ -167,6 +257,10 @@ const styles = StyleSheet.create({
   questionIcon: {
     fontSize: 60,
     color: Colors.almostWhite,
+  },
+  map: {
+    height: 130,
+    borderRadius: Spacing.smaller,
   },
 });
 
