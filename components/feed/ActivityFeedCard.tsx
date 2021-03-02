4a8aa0d1662765bdc97d327409439321cc44c4c6
Mats Tyldum <ma.tyldum@gmail.com>
2021-03-02 15:02:02 +0100

    New activity feed card layout

--- components/feed/ActivityFeedCard.tsx
+++ components/feed/ActivityFeedCard.tsx
@@ -5,16 +5,24 @@ import { ActivityFeedData } from '../../types/feedTypes';
 import { timeStampToPresentable } from '../../helpers/dateTimeHelpers';
 import MapView, { LatLng, Marker, Region } from 'react-native-maps';
 import GeoFences from '../map/GeoFences';
-import { getGeoFenceImage } from '../../helpers/geoFenceCalculations';
+import { getGeoFenceColor, getGeoFenceImage } from '../../helpers/geoFenceCalculations';
 import { convertToGeoFence, defaultUserProfile } from '../../helpers/objectMappers';
 import { GeoFenceCategory } from '../../types/geoFenceTypes';
 import TouchableProfile from '../general/TouchableProfile';
+import Divider from '../general/Divider';
+import { FontAwesome5 as FAIcon } from '@expo/vector-icons';
+import { Avatar } from 'react-native-elements';
+import useAuthentication from '../../hooks/useAuthentication';
+import { gray800 } from '../../theme/colors';
+import { Asset } from 'expo-asset';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 interface ActivityFeedCardProps {
   data: ActivityFeedData;
 }
 
 const ActivityFeedCard: React.FC<ActivityFeedCardProps> = ({ data }: ActivityFeedCardProps) => {
+  const auth = useAuthentication();
   const mapRegion: Region = {
     latitude: data.activity.geofence.latitude,
     longitude: data.activity.geofence.longitude,
@@ -26,20 +34,58 @@ const ActivityFeedCard: React.FC<ActivityFeedCardProps> = ({ data }: ActivityFee
     longitude: data.activity.geofence.longitude,
   };
   const activityGeoFence = convertToGeoFence(data.activity.geofence);
+  const getName = () => {
+    if (auth.user?.uid === data.user.id) return 'You';
+    return data.user.name;
+  };
   return (
     <View style={styles.card}>
       <TouchableProfile user_id={data.user.id} name={data.user.name}>
-        <View style={styles.topBar}>
-          <Image
-            source={{ uri: data.user.picture ? data.user.picture : defaultUserProfile.picture }}
-            style={styles.avatar}
-          />
-          <View>
-            <Text style={styles.nameText}>{data.user.name}</Text>
-            <Text style={styles.captionText}>{data.activity.caption}</Text>
+        <View style={styles.flexRowSpaceBetween}>
+          <View style={styles.topBar}>
+            <Avatar
+              source={{ uri: data.user.picture ? data.user.picture : defaultUserProfile.picture }}
+              size={'medium'}
+            />
+            <View style={{ marginLeft: Spacing.smaller }}>
+              <Text style={styles.nameText}>{getName()}</Text>
+              <Text style={styles.captionText}>{data.activity.caption}</Text>
+            </View>
           </View>
+
+          <FAIcon name={'chevron-right'} style={{ ...Typography.icon, paddingRight: Spacing.smaller }} />
         </View>
       </TouchableProfile>
+      <Divider />
+      <View style={styles.innerCard}>
+        <View>
+          <View style={[styles.flexRowLeft, { marginBottom: Spacing.smaller }]}>
+            <FAIcon name={'map-marker-alt'} style={[styles.label, { marginRight: Spacing.smaller }]} />
+            <Text style={styles.label}>{data.activity.geofence.name}</Text>
+          </View>
+          <View style={styles.flexRowLeft}>
+            <FAIcon name={'stopwatch'} style={[styles.label, { marginRight: Spacing.smaller }]} />
+            <Text style={styles.label}>{data.activity.duration}</Text>
+          </View>
+        </View>
+        {data.activity.friend && (
+          <View>
+            <View style={[styles.collabIcon, { marginBottom: Spacing.smaller }]}>
+              <Text style={styles.collabIconText}>2x points</Text>
+            </View>
+            <View>
+              <TouchableProfile user_id={data.activity.friend.id} name={data.activity.friend.name}>
+                <View style={styles.flexRowSpaceEven}>
+                  <Text style={{ ...Typography.largeBodyText, marginRight: Spacing.smallest, fontWeight: 'bold' }}>
+                    With
+                  </Text>
+                  <Avatar source={{ uri: data.activity.friend.picture ?? defaultUserProfile.picture }} size={'small'} />
+                </View>
+              </TouchableProfile>
+            </View>
+          </View>
+        )}
+      </View>
       <View style={styles.main}>
         <View style={styles.category}>
           <Image
@@ -64,6 +110,13 @@ const ActivityFeedCard: React.FC<ActivityFeedCardProps> = ({ data }: ActivityFee
         </MapView>
       </View>
       <View style={styles.footer}>
+        <TouchableOpacity>
+          <Image
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            source={{ uri: Asset.fromModule(require('../../assets/images/clap.png')).uri }}
+            style={styles.reactionIcon}
+          />
+        </TouchableOpacity>
         <Text style={styles.footerText}>{timeStampToPresentable(data.createdAt)}</Text>
       </View>
     </View>
@@ -79,6 +132,8 @@ const styles = StyleSheet.create({
   topBar: {
     flexDirection: 'row',
     justifyContent: 'flex-start',
+    borderRadius: Spacing.smaller,
+    padding: Spacing.smallest,
   },
   nameText: {
     ...Typography.headerText,
@@ -103,12 +158,14 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   category: {
-    width: '30%',
+    width: '29%',
+    height: 120,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'column',
-    paddingRight: Spacing.base,
+    paddingHorizontal: Spacing.smaller,
+    borderRadius: Spacing.smaller,
   },
   categoryIcon: {
     height: 50,
@@ -116,13 +173,15 @@ const styles = StyleSheet.create({
     marginVertical: Spacing.smallest,
   },
   map: {
-    width: '70%',
-    height: 110,
+    width: '69%',
+    height: 120,
     borderRadius: Spacing.smallest,
   },
   footer: {
     flexDirection: 'row',
-    justifyContent: 'flex-end',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: Spacing.smaller,
     width: '100%',
   },
   footerText: {
@@ -135,6 +194,50 @@ const styles = StyleSheet.create({
     width: 45,
     borderRadius: 45 / 2,
     marginRight: Spacing.small,
+  },
+  // New
+  reactionIcon: {
+    height: 25,
+    width: 25,
+    marginVertical: Spacing.smallest,
+  },
+  label: {
+    ...Typography.largeBodyText,
+    fontWeight: 'bold',
+  },
+  flexRowLeft: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  flexRowSpaceEven: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  flexRowSpaceBetween: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  innerCard: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: Spacing.smallest,
+  },
+  collabIcon: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    backgroundColor: Colors.gray900,
+    borderColor: Colors.gold,
+    borderRadius: Spacing.smaller,
+    padding: Spacing.smaller,
+  },
+  collabIconText: {
+    ...Typography.largeBodyText,
+    fontWeight: 'bold',
+    color: Colors.gold,
   },
 });
 
