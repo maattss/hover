0baff76e1f8906aab79d1ec4bb91fc80d2e1ca46
Siri Mykland <sirim_mykland@hotmail.com>
2021-03-02 10:21:56 +0100

    added small vertical padding

--- components/feed/notification/NotificationCard.tsx
+++ components/feed/notification/NotificationCard.tsx
@@ -106,7 +106,7 @@ const styles = StyleSheet.create({
   },
   goTo: {
     width: '5%',
-    paddingLeft: Spacing.small,
+    paddingVertical: Spacing.small,
     flexDirection: 'row',
     justifyContent: 'flex-end',
     alignItems: 'center',
