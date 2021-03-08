c3bb202f0549dc8ffd006d43b60955331dfe26af
Mats Tyldum <ma.tyldum@gmail.com>
2021-03-08 09:47:25 +0100

    Update challenge screen

--- screens/challenge/newchallenge/ChallengeInfoScreen.tsx
+++ screens/challenge/newchallenge/ChallengeInfoScreen.tsx
@@ -1,27 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Spacing, Typography } from '../../../theme';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Spacing, Typography, Colors } from '../../../theme';
+import { FontAwesome as FAIcon } from '@expo/vector-icons';
 
 const ChallengeInfoScreen: React.FC = () => {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.titleText}>Challenge information</Text>
       <Text style={styles.bodyText}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis sapiente error maiores, corporis molestias
-        mollitia numquam voluptatem, sunt corrupti fugiat inventore hic doloribus, quidem odit quas consectetur eius
-        labore ad!
-        {/* TODO: Insert challenge description */}
+        Create a challenge to compete with your friends. Below are some information about the four steps you need to
+        complete before the challenge is created.
       </Text>
-    </View>
+      <Text style={styles.headerText}>Choose opponent</Text>
+      <Text style={styles.bodyText}>Select one or more people you want to compete against.</Text>
+      <Text style={styles.headerText}>Choose challenge type</Text>
+      <Text style={styles.bodyText}>
+        Choose which type of challenge you want to create. There are four possibilities:
+      </Text>
+      <View style={styles.listItem}>
+        <FAIcon name={'circle'} style={styles.bulletPoint} />
+        <Text style={styles.listText}>Total time in category</Text>
+      </View>
+      <View style={styles.listItem}>
+        <FAIcon name={'circle'} style={styles.bulletPoint} />
+        <Text style={styles.listText}>Total time</Text>
+      </View>
+      <View style={styles.listItem}>
+        <FAIcon name={'circle'} style={styles.bulletPoint} />
+        <Text style={styles.listText}>Total score</Text>
+      </View>
+      <View style={styles.listItem}>
+        <FAIcon name={'circle'} style={styles.bulletPoint} />
+        <Text style={styles.listText}>Total score in category</Text>
+      </View>
+      <Text style={styles.headerText}>Define details</Text>
+      <Text style={styles.bodyText}>
+        Determine the goal of the challenge and how lonelong the challenge should last. The details are dependent on
+        which type of challenge you selected.
+      </Text>
+      <Text style={styles.headerText}>Overview</Text>
+      <Text style={styles.bodyText}>Take a last look at your challenge before completing the creation.</Text>
+    </ScrollView>
   );
 };
 export default ChallengeInfoScreen;
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    alignItems: 'center',
-    paddingHorizontal: Spacing.base,
+    paddingHorizontal: Spacing.large,
   },
   titleText: {
     ...Typography.headerText,
@@ -31,6 +57,25 @@ const styles = StyleSheet.create({
   },
   bodyText: {
     ...Typography.bodyText,
+    paddingTop: Spacing.smallest,
+  },
+  headerText: {
+    ...Typography.largeBodyText,
+    fontWeight: 'bold',
     paddingTop: Spacing.base,
+  },
+  listItem: {
+    flexDirection: 'row',
+    marginTop: Spacing.smaller,
+  },
+  listText: {
+    ...Typography.bodyText,
+  },
+  bulletPoint: {
+    color: Colors.almostWhite,
+    fontSize: 8,
+    marginRight: Spacing.smaller,
+    paddingTop: Spacing.smallest,
+    marginLeft: Spacing.smaller,
   },
 });
