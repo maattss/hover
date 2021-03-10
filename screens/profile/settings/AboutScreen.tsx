48e190beed9e05bd026a6bbcd3e61c84e74a149f
Siri Mykland <sirim_mykland@hotmail.com>
2021-03-10 18:42:22 +0100

    centered text in about page

--- screens/profile/settings/AboutScreen.tsx
+++ screens/profile/settings/AboutScreen.tsx
@@ -41,6 +41,7 @@ const AboutScreen: React.FC<SettingsProps> = () => {
       <View style={styles.versionFooter}>
         <Text style={styles.versionText}>Version {Constants.manifest.version}</Text>
       </View>
+      <Text style={styles.versionText}>Version {Constants.manifest.version} (beta)</Text>
     </View>
   );
 };
@@ -61,6 +62,7 @@ const styles = StyleSheet.create({
   bodyText: {
     ...Typography.bodyText,
     paddingBottom: Spacing.smaller,
+    textAlign: 'center',
   },
   centeredContainer: {
     marginVertical: Spacing.base,
