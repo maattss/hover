8cd5afd8881c91474f949eaa2f3024058348dc08
Mats Tyldum <ma.tyldum@gmail.com>
2021-03-12 12:28:04 +0100

    Reset navigation to profile screen when user clicks on their touchable profile

--- components/general/TouchableProfile.tsx
+++ components/general/TouchableProfile.tsx
@@ -3,9 +3,10 @@ import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native';
 import useAuthentication from '../../hooks/useAuthentication';
-import { RootTabParamList } from '../../types/navigationTypes';
+import { ProfileStackParamList, RootTabParamList } from '../../types/navigationTypes';
 
 type BottomNavigationProp = BottomTabNavigationProp<RootTabParamList>;
+type ProfileNavigationProp = BottomTabNavigationProp<ProfileStackParamList>;
 
 type Props = {
   children: React.ReactChild;
@@ -18,10 +19,14 @@ const TouchableProfile: React.FC<Props> = ({ user_id, children, name, onPress }:
   const bottomNavigation = useNavigation<BottomNavigationProp>();
   const id = useAuthentication().user?.uid;
   const navigation = useNavigation();
+  const profileNavigator = useNavigation<ProfileNavigationProp>();
 
   const navigate = () => {
     if (user_id == id) {
       bottomNavigation.navigate('Profile');
+      profileNavigator.reset({
+        routes: [{ name: 'Profile' }],
+      });
     } else {
       navigation.navigate('UserProfile', {
         user_id: user_id,
