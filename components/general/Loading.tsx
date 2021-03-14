604c02c36da9a7ad4bb7a1855c47aa5f35603973
Mats Tyldum <ma.tyldum@gmail.com>
2021-03-14 20:21:53 +0100

    Update Loading.tsx

--- components/general/Loading.tsx
+++ components/general/Loading.tsx
@@ -26,7 +26,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     height: '100%',
     width: '100%',
-    marginTop: '20%',
+    marginTop: '10%',
   },
   loadingText: {
     ...Typography.bodyText,
